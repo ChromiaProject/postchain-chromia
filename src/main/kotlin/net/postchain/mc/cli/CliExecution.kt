526cff9b705f9eaa748919e184078675d5ac3006
Dang Viet Ha <ha@chromaway.com>
2019-11-22 21:26:36 +0700

    check require parameters in app config file

--- src/main/kotlin/net/postchain/mc/cli/CliExecution.kt
+++ src/main/kotlin/net/postchain/mc/cli/CliExecution.kt
@@ -27,6 +27,9 @@ class CliExecution {
     private fun getPostchainClient(configFile: String): PostchainClient {
         val config = AppConfig.fromPropertiesFile(configFile)
 
+        if (config.adminPrivKey.isEmpty() || config.brid.isEmpty() || config.adminPubKey.isEmpty() || config.adminPrivKey.isEmpty()) {
+            throw UserMistake("missing required parameters")
+        }
         val resolver = postchainClientFactory.makeSimpleNodeResolver(config.apiURL)
         val sigMaker = cryptoSystem.buildSigMaker(config.adminPubKey.hexStringToByteArray(), config.adminPrivKey.hexStringToByteArray())
         return postchainClientFactory.getClient(resolver, config.brid.hexStringToByteArray(), DefaultSigner(sigMaker, config.adminPubKey.hexStringToByteArray()))
@@ -41,17 +44,28 @@ class CliExecution {
      *
      */
     fun addBlockchainConfiguration(configFile: String, brid: String, height: Long, blockchainConfigFile: String, signer: Pair<ByteArray, ByteArray>) {
-        val data = getEncodedGtxValueFromFile(blockchainConfigFile)
-        val client = getPostchainClient(configFile)
-        val tx = client.makeTransaction()
-        tx.addOperation("add_blockchain_configuration",
-                arrayOf(GtvFactory.gtv(brid.hexStringToByteArray()), GtvFactory.gtv(height), GtvFactory.gtv(data)))
-        tx.sign(cryptoSystem.buildSigMaker(signer.first, signer.second))
-        val txResult = tx.postSync(ConfirmationLevel.UNVERIFIED)
-        if (txResult.status == TransactionStatus.CONFIRMED) {
-            println("blockchain configuration at $height was added successfully!")
-        } else {
-            throw CliError.Companion.CliException("Cannot add blockchain configuration at $height ")
+        try {
+            val data = getEncodedGtxValueFromFile(blockchainConfigFile)
+            val client = getPostchainClient(configFile)
+            val tx = client.makeTransaction()
+            tx.addOperation("add_blockchain_configuration",
+                    arrayOf(GtvFactory.gtv(brid.hexStringToByteArray()), GtvFactory.gtv(height), GtvFactory.gtv(data)))
+            tx.sign(cryptoSystem.buildSigMaker(signer.first, signer.second))
+            val txResult = tx.postSync(ConfirmationLevel.UNVERIFIED)
+            if (txResult.status == TransactionStatus.CONFIRMED) {
+                println("blockchain configuration at $height was added successfully!")
+            } else {
+                throw CliError.Companion.CliException("Cannot add blockchain configuration at $height ")
+            }
+        } catch (e: ConfigurationException) {
+            logger.error(e.message)
+            throw CliError.Companion.CliException("Config file not found $configFile")
+        } catch (e: UserMistake) {
+            logger.error(e.message)
+            throw CliError.Companion.CliException("User Mistake: Input parameters might be wrong or missing")
+        } catch (e: Exception) {
+            logger.error(e.message)
+            throw CliError.Companion.CliException("System Error: Something wrong happen")
         }
     }
 
@@ -76,7 +90,7 @@ class CliExecution {
             throw CliError.Companion.CliException("Config file not found $configFile")
         } catch (e: UserMistake) {
             logger.error(e.message)
-            throw CliError.Companion.CliException("User Mistake: Input parameters might be wrong")
+            throw CliError.Companion.CliException("User Mistake: Input parameters might be wrong or missing")
         } catch (e: Exception) {
             logger.error(e.message)
             throw CliError.Companion.CliException("System Error: Something wrong happen")
@@ -104,7 +118,7 @@ class CliExecution {
             throw CliError.Companion.CliException("Config file not found $configFile")
         } catch (e: UserMistake) {
             logger.error(e.message)
-            throw CliError.Companion.CliException("User Mistake: Input parameters might be wrong")
+            throw CliError.Companion.CliException("User Mistake: Input parameters might be wrong or missing")
         } catch (e: Exception) {
             logger.error(e.message)
             throw CliError.Companion.CliException("System Error: Something wrong happen")
@@ -115,15 +129,26 @@ class CliExecution {
      *
      */
     fun addSystemPeer(configFile: String, key: String, signer: Pair<ByteArray, ByteArray>) {
-        val client = getPostchainClient(configFile)
-        val tx = client.makeTransaction()
-        tx.addOperation("add_system_peer",
-                arrayOf(GtvFactory.gtv(key.hexStringToByteArray())))
-        tx.sign(cryptoSystem.buildSigMaker(signer.first, signer.second))
-        tx.post(ConfirmationLevel.VERIFIED).fail {
-            throw CliError.Companion.CliException("Cannot add system peer")
-        }.success {
-            println("System peer has been added")
+        try {
+            val client = getPostchainClient(configFile)
+            val tx = client.makeTransaction()
+            tx.addOperation("add_system_peer",
+                    arrayOf(GtvFactory.gtv(key.hexStringToByteArray())))
+            tx.sign(cryptoSystem.buildSigMaker(signer.first, signer.second))
+            tx.post(ConfirmationLevel.VERIFIED).fail {
+                throw CliError.Companion.CliException("Cannot add system peer")
+            }.success {
+                println("System peer has been added")
+            }
+        } catch (e: ConfigurationException) {
+            logger.error(e.message)
+            throw CliError.Companion.CliException("Config file not found $configFile")
+        } catch (e: UserMistake) {
+            logger.error(e.message)
+            throw CliError.Companion.CliException("User Mistake: Input parameters might be wrong or missing")
+        } catch (e: Exception) {
+            logger.error(e.message)
+            throw CliError.Companion.CliException("System Error: Something wrong happen")
         }
     }
 }
