02e0d3769ce7ece7f7b944954e7652e7fe1e67b6
Eugene Tykulov <eugene.tykulov@gmail.com>
2021-01-11 17:02:57 +0200

    POS-129: Compatibility with postchain-3.3.1-SNAPSHOTS (container-chains)

--- src/main/kotlin/net/postchain/mc/test/PostchainTestNode.kt
+++ src/main/kotlin/net/postchain/mc/test/PostchainTestNode.kt
@@ -4,6 +4,7 @@ import mu.KLogging
 import net.postchain.PostchainNode
 import net.postchain.StorageBuilder
 import net.postchain.api.rest.controller.Model
+import net.postchain.api.rest.infra.BaseApiInfrastructure
 import net.postchain.base.*
 import net.postchain.base.data.BaseBlockchainConfiguration
 import net.postchain.base.data.DatabaseAccess
@@ -66,7 +67,7 @@ class PostchainTestNode(nodeConfigProvider: NodeConfigurationProvider, preWipeDa
     fun getRestApiModel(): Model {
         val blockchainProcess = processManager.retrieveBlockchain(DEFAULT_CHAIN_IID)!!
         return ((blockchainInfrastructure as BaseBlockchainInfrastructure).apiInfrastructure as BaseApiInfrastructure)
-                .restApi?.retrieveModel(blockchainRID(blockchainProcess))!!
+                .restApi?.retrieveModel(blockchainRID(blockchainProcess)) as Model
     }
 
     fun getRestApiHttpPort(): Int {
