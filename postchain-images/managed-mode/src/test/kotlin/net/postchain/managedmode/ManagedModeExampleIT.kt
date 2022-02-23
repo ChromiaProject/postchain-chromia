23ab1b01d671ca98aa1a91488922e62162e0ef9b
Joar Svartholm <joar.svartholm@chromaway.com>
2022-02-23 16:18:36 +0100

    parse config file in container and add initial node to postchain network

--- postchain-images/managed-mode/src/test/kotlin/net/postchain/managedmode/ManagedModeExampleIT.kt
+++ postchain-images/managed-mode/src/test/kotlin/net/postchain/managedmode/ManagedModeExampleIT.kt
@@ -1,14 +1,14 @@
 package net.postchain.managedmode
 
 import assertk.assert
+import assertk.assertions.isEqualTo
 import assertk.assertions.isZero
-import net.postchain.dapp.PostchainContainer
-import net.postchain.dapp.startContainers
-import net.postchain.dapp.stopContainers
+import net.postchain.common.hexStringToByteArray
+import net.postchain.dapp.*
+import net.postchain.dapp.PostchainContainer.Companion.POSTCHAIN_PATH
+import net.postchain.gtv.GtvFactory.gtv
 import net.postchain.postgres.ChromaWayPostgresContainer
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import org.testcontainers.containers.BindMode
 import org.testcontainers.containers.Network
 import org.testcontainers.junit.jupiter.Container
@@ -16,6 +16,7 @@ import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
 
 @Testcontainers
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 internal class ManagedModeExampleIT {
 
     companion object {
@@ -29,32 +30,60 @@ internal class ManagedModeExampleIT {
                 .withNetwork(network)
 
 
-        private val node1 = PostchainContainer(imageName)
+        private val node1 = PostchainContainer(imageName, parseConfig(this::class.java.getResource("/managed-mode-example/node1/node-config.properties")!!.file))
                 .withNetwork(network)
                 .withNetworkAliases("node1")
-                .withClasspathResourceMapping("$resourceFolder/node1", "${PostchainContainer.POSTCHAIN_PATH}/config", BindMode.READ_ONLY)
+                .withClasspathResourceMapping("$resourceFolder/node1", "${POSTCHAIN_PATH}/config", BindMode.READ_ONLY)
                 .withEnv("POSTCHAIN_DB_URL", postgres.networkJdbcUrl())
                 .withEnv("NODE_PUBKEY", "0350fe40766bc0ce8d08b3f5b810e49a8352fdd458606bd5fafe5acdcdc8ff3f57")
                 .withEnv("NODE_HOST", "node1")
                 .withEnv("NODE_PORT", "9871")
+                .withEnv("RELL_OUT", "${POSTCHAIN_PATH}/chain0-generated")
                 .withEnv("WIPE_DB", "true")
                 .withFixedExposedPort(9871, 9871)
+                .withEnv("JAVA_OPTS", "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:5005")
+
+        private val node2 = PostchainContainer(imageName, parseConfig(this::class.java.getResource("/managed-mode-example/node2/node-config.properties")!!.file))
+                .withNetwork(network)
+                .withNetworkAliases("node2")
+                .withClasspathResourceMapping("$resourceFolder/node2", "${POSTCHAIN_PATH}/config", BindMode.READ_ONLY)
+                .withEnv("POSTCHAIN_DB_URL", postgres.networkJdbcUrl())
+                .withEnv("NODE_PUBKEY", "02B99A05912B01B7797D84D6660E9ED35FAEE078BD5BDF40026E0CC6E0CB2EF50C")
+                .withEnv("NODE_HOST", "node2")
+                .withEnv("NODE_PORT", "9872")
+                .withEnv("RELL_OUT", "${POSTCHAIN_PATH}/chain0-generated")
+                .withEnv("WIPE_DB", "true")
+                .withFixedExposedPort(9872, 9872)
+
+        private val node3 = PostchainContainer(imageName, parseConfig(this::class.java.getResource("/managed-mode-example/node3/node-config.properties")!!.file))
+                .withNetwork(network)
+                .withNetworkAliases("node3")
+                .withClasspathResourceMapping("$resourceFolder/node3", "${POSTCHAIN_PATH}/config", BindMode.READ_ONLY)
+                .withEnv("POSTCHAIN_DB_URL", postgres.networkJdbcUrl())
+                .withEnv("NODE_PUBKEY", "02839DDE1D2121CE72794E54180F5F5C3AD23543D419CB4C3640A854ACB1ADA9E6")
+                .withEnv("NODE_HOST", "node3")
+                .withEnv("NODE_PORT", "9873")
+                .withEnv("RELL_OUT", "${POSTCHAIN_PATH}/chain0-generated")
+                .withEnv("WIPE_DB", "true")
+                .withFixedExposedPort(9873, 9873)
 
         @JvmStatic
         @BeforeAll
         fun setup() {
-            startContainers(node1)
+            println("Starting nodes...")
+            startContainers(node1, node2, node3)
         }
 
 
         @JvmStatic
         @AfterAll
         fun breakdown() {
-            stopContainers(node1)
+            stopContainers(node1, node2, node3)
         }
     }
 
     @Test
+    @Order(1)
     fun `Chain0 dapp is deployed`() {
         assert(
                 node1.execInContainer("ls", "/opt/chromaway/postchain/chain0-generated/blockchains/0").exitCode
@@ -62,8 +91,23 @@ internal class ManagedModeExampleIT {
     }
 
     @Test
-    fun `Managed mode example`() {
+    @Order(2)
+    fun `Add node to the network`() {
+        val db = postgres.createChainDatabaseCommunicator(0, node1.appConfig.databaseSchema)
+        println("Registering provider")
+        node1.tx(0, "register_provider", gtv(adminPubKey.hexStringToByteArray()))
+        db.awaitNewBlock()
+        println("done")
 
+        val provider = node1.client(0).query("get_provider", gtv("pubkey" to gtv(adminPubKey.hexStringToByteArray()))).get()
+        println("Enabling provider")
+        node1.tx(0, "enable_provider", provider)
+        db.awaitNewBlock()
+
+        println("Adding node")
+        node1.tx(0, "add_node", provider, gtv(node1.pubKey.hexStringToByteArray()), gtv("node1"), gtv(9871))
+        db.awaitNewBlock()
+        val nodeGtv = node1.client(0).query("get_node_data", gtv("pubkey" to gtv(node1.pubKey.hexStringToByteArray()))).get()
+        assert(nodeGtv.asDict()["active"]!!.asInteger()).isEqualTo(1)
     }
-
 }
