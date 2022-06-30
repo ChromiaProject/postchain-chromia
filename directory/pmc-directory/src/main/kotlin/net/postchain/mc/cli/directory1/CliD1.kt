c6e537e6fb1e6dd4943019c8987889eea3558c86
Joar Svartholm <joar.svartholm@chromaway.com>
2022-06-30 08:26:36 +0200

    Remove KeyGen command

--- directory/pmc-directory/src/main/kotlin/net/postchain/mc/cli/directory1/CliD1.kt
+++ directory/pmc-directory/src/main/kotlin/net/postchain/mc/cli/directory1/CliD1.kt
@@ -1,17 +1,13 @@
 package net.postchain.mc.cli.directory1
 
 import com.beust.jcommander.JCommander
-import net.postchain.mc.cli.account.CommandKeygen
 import net.postchain.mc.cli.base.CliBase
 import net.postchain.mc.cli.base.Command
-import net.postchain.mc.cli.provider.*
 import net.postchain.mc.cli.replica.*
 import net.postchain.mc.cli.votingupdates.*
 
 class CliD1: CliBase() {
     override val commands: Map<String, Command> = listOf(
-            CommandKeygen(),
-
             // Clusters
             CommandProposeClusterProvider(),
             CommandProposeClusterResourceLimits(),
