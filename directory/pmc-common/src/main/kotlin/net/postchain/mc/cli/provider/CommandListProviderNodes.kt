1bca6538db59b4dc720273c6b03e454c84645e7f
Joar Svartholm <joar.svartholm@chromaway.com>
2022-09-09 16:42:15 +0200

    Update output from provider commands

--- directory/pmc-common/src/main/kotlin/net/postchain/mc/cli/provider/CommandListProviderNodes.kt
+++ directory/pmc-common/src/main/kotlin/net/postchain/mc/cli/provider/CommandListProviderNodes.kt
@@ -3,6 +3,7 @@ package net.postchain.mc.cli.provider
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
+import de.m3y.kformat.table
 import net.postchain.chain0.common.queries.getNodesByProvider
 import net.postchain.cli.util.requiredPubkeyOption
 import net.postchain.common.hexStringToByteArray
@@ -10,6 +11,7 @@ import net.postchain.common.toHex
 import net.postchain.mc.cli.base.ClientUtil
 import net.postchain.mc.cli.includeInactiveOption
 import net.postchain.mc.cli.util.configOption
+import java.lang.StringBuilder
 import java.time.Instant
 import java.util.Date
 
@@ -21,24 +23,23 @@ class CommandListProviderNodes : CliktCommand(
 
     private val key by requiredPubkeyOption()
 
-    private val verbose by option("-v", "--verbose", help = "Show verbose output").flag()
-
-    private val includeInactive by includeInactiveOption()
-
     override fun run() {
-        ClientUtil.fromConfig(config).getNodesByProvider(key.hexStringToByteArray()).joinToString("\n") {
-            if (!verbose) {
-                it.pubkey.toHex()
-            } else {
-                """
-                    Node: ${it.pubkey.toHex()}
-                    Host: ${it.host}
-                    Port: ${it.port}
-                    Active: ${it.active}
-                    Last updated: ${Date.from(Instant.ofEpochMilli(it.lastUpdated))}
-                    -----------------------------------------------
-                """.trimIndent()
+        println("Nodes for provider $key")
+        table {
+            header("Pubkey", "Host", "Port", "Api port", "Active", "Last updated")
+
+            ClientUtil.fromConfig(config).getNodesByProvider(key.hexStringToByteArray()).forEach {
+                row(
+                    it.pubkey.toHex(),
+                    it.host,
+                    it.port,
+                    "",
+                    it.active.toString(),
+                    Date.from(Instant.ofEpochMilli(it.lastUpdated))
+                )
             }
-        }.let { println(it) }
+        }
+            .render(StringBuilder())
+            .also { println(it) }
     }
 }
