1f0aea51b895118fe1c869f2333c367ae9bafabd
Joar Svartholm <joar.svartholm@chromaway.com>
2022-09-14 12:44:01 +0200

    add message to require_provider

--- directory/pmc-directory/src/main/kotlin/net/postchain/mc/cli/cluster/CommandAddCluster.kt
+++ directory/pmc-directory/src/main/kotlin/net/postchain/mc/cli/cluster/CommandAddCluster.kt
@@ -5,22 +5,17 @@ import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.options.split
-import com.sun.net.httpserver.Authenticator.Success
 import net.postchain.chain0.common.createClusterOperation
 import net.postchain.common.hexStringToByteArray
-import net.postchain.common.tx.TransactionStatus
 import net.postchain.mc.cli.base.ClientUtil
 import net.postchain.mc.cli.base.printResult
 import net.postchain.mc.cli.base.pubkey
-import net.postchain.mc.cli.common0.CliExecution
-import net.postchain.mc.cli.util.NopPostchainClient
 import net.postchain.mc.cli.util.configOption
 import net.postchain.mc.cli.util.nameOrGenerateOption
-import net.postchain.mc.cli.util.nopClientOption
 
 class CommandAddCluster : CliktCommand(
-    name = "add",
-    help = "Create a new cluster that can hold containers with blockchains."
+        name = "add",
+        help = "Create a new cluster that can hold containers with blockchains."
 ) {
 
     private val config by configOption()
@@ -28,28 +23,28 @@ class CommandAddCluster : CliktCommand(
     private val name by nameOrGenerateOption("Cluster name")
 
     private val providers by option(
-        "-p", "--providers",
-        help = "String of comma separated list of pubkey strings of providers that should belong to this cluster"
+            "-p", "--providers",
+            help = "String of comma separated list of pubkey strings of providers that should belong to this cluster"
     ).convert { it.hexStringToByteArray() }.split(",").required()
 
     private val governorName by option(
-        "-g", "--governor",
-        help = "Name of another voter set which can update this cluster."
+            "-g", "--governor",
+            help = "Name of another voter set which can update this cluster."
     ).required()
 
     private val deployerName by option(
-        "-d", "--deployer",
-        help = "Name of the voter set which can make updates in this cluster"
+            "-d", "--deployer",
+            help = "Name of the voter set which can make updates in this cluster"
     ).required()
 
     override fun run() {
         val client = ClientUtil.nopClientFromConfig(config)
         client.transactionBuilder()
-            .createClusterOperation(config.pubkey().key, name, providers, governorName, deployerName)
-            .postSyncAwaitConfirmation()
-            .printResult(
-                "Cluster $name added",
-                "Could not create cluster"
-            )
+                .createClusterOperation(config.pubkey().key, name, providers, governorName, deployerName)
+                .postSyncAwaitConfirmation()
+                .printResult(
+                        "Cluster $name added",
+                        "Could not create cluster"
+                )
     }
 }
