9aeeb3596eeda55826b8ecc5563d96fbdd80d877
Joar Svartholm <joar.svartholm@chromaway.com>
2022-09-16 09:57:43 +0200

    Spawn clients for a blockchain in a cluster

--- chromia-client/src/main/kotlin/net/postchain/client/chromia/ChromiaClientProvider.kt
+++ chromia-client/src/main/kotlin/net/postchain/client/chromia/ChromiaClientProvider.kt
@@ -6,24 +6,46 @@ import net.postchain.client.core.PostchainClient
 import net.postchain.client.request.EndpointPool
 import net.postchain.common.BlockchainRid
 import net.postchain.gtv.GtvFactory.gtv
+import org.http4k.client.ApacheAsyncClient
 
 class ChromiaClientProvider(val config: PostchainClientConfig) {
-    private val chain0Client = ConcretePostchainClient(config)
+    private val httpClient = ApacheAsyncClient()
+    private val chain0Client = ConcretePostchainClient(config, httpClient)
 
-    fun fromCluster(cluster: String, brid: BlockchainRid): PostchainClient {
-        val apiUrls = chain0Client.querySync("get_cluster_api_urls", gtv("name" to gtv(cluster))).asArray().map { it.asString() }
-        val chainClientConfig = PostchainClientConfig(
-            blockchainRid = brid,
-            endpointPool = EndpointPool.default(apiUrls),
-            signers = config.signers,
-            statusPollCount = config.statusPollCount,
-            statusPollInterval = config.statusPollInterval
-        )
-        return ConcretePostchainClient(chainClientConfig)
+    fun clusters(): List<String> {
+        return chain0Client.querySync("list_clusters").asArray().map { it.asString() }
     }
 
-    fun fromBlockchainRid(brid: BlockchainRid): PostchainClient {
-        val cluster = chain0Client.querySync("get_blockchain_cluster", gtv(brid)).asString()
-        return fromCluster(cluster, brid)
+    fun cluster(cluster: String): ClusterPostchainClient {
+        val apiUrls = chain0Client.querySync("get_cluster_api_urls", gtv("name" to gtv(cluster))).asArray().map { it.asString() }
+        return ClusterPostchainClient(cluster, EndpointPool.default(apiUrls))
+    }
+
+    fun blockchain(brid: BlockchainRid): PostchainClient {
+        val apiUrls = chain0Client.querySync("get_blockchain_api_urls", gtv(brid)).asArray().map { it.asString() }
+        return ConcretePostchainClient(
+            config.copy(
+                blockchainRid = brid,
+                endpointPool = EndpointPool.default(apiUrls)
+            ),
+            httpClient
+        )
+    }
+
+    inner class ClusterPostchainClient(private val name: String, private val endpointPool: EndpointPool) {
+
+        fun blockchains(): List<BlockchainRid> {
+            return chain0Client.querySync("get_cluster_blockchains", gtv(name)).asArray().map { BlockchainRid(it.asByteArray()) }
+        }
+
+        fun blockchain(brid: BlockchainRid): PostchainClient {
+            return ConcretePostchainClient(
+                config.copy(
+                    blockchainRid = brid,
+                    endpointPool = endpointPool
+                ),
+                httpClient
+            )
+        }
     }
 }
