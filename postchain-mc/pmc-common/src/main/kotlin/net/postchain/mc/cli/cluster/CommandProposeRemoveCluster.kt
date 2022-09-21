1aaf34162b2408efa10bb290159214b91d49ccfa
Eugene Tykulov <eugene.tykulov@chromaway.com>
2022-09-21 17:08:45 +0300

    POS-281: Review comments

--- postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/cluster/CommandProposeRemoveCluster.kt
+++ postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/cluster/CommandProposeRemoveCluster.kt
@@ -3,24 +3,22 @@ package net.postchain.mc.cli.cluster
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.required
 import net.postchain.chain0.common.proposal.proposeRemoveClusterOperation
-import net.postchain.mc.cli.base.ClientUtil
 import net.postchain.mc.cli.base.printResult
 import net.postchain.mc.cli.base.pubkey
-import net.postchain.mc.cli.util.configOption
 import net.postchain.mc.cli.util.nameOption
+import net.postchain.mc.cli.util.nopClientOption
 
 class CommandProposeRemoveCluster : CliktCommand(
         name = "remove",
         help = "Propose removal of cluster. Command is irreversible"
 ) {
-    private val config by configOption()
+    private val client by nopClientOption()
 
-    private val name by nameOption("Cluster name").required()
+    private val name by nameOption("Cluster name to remove").required()
 
     override fun run() {
-        ClientUtil.nopClientFromConfig(config)
-                .transactionBuilder()
-                .proposeRemoveClusterOperation(config.pubkey().key, name)
+        client.transactionBuilder()
+                .proposeRemoveClusterOperation(client.config.pubkey().key, name)
                 .postSyncAwaitConfirmation()
                 .printResult(
                         "Cluster removal proposed",
--- postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/container/CommandProposeRemoveContainer.kt
+++ postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/container/CommandProposeRemoveContainer.kt
@@ -3,24 +3,22 @@ package net.postchain.mc.cli.container
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.required
 import net.postchain.chain0.common.proposal.proposeRemoveContainerOperation
-import net.postchain.mc.cli.base.ClientUtil
 import net.postchain.mc.cli.base.printResult
 import net.postchain.mc.cli.base.pubkey
-import net.postchain.mc.cli.util.configOption
 import net.postchain.mc.cli.util.nameOption
+import net.postchain.mc.cli.util.nopClientOption
 
 class CommandProposeRemoveContainer : CliktCommand(
         name = "remove",
         help = "Propose removal of container. Command is irreversible"
 ) {
-    private val config by configOption()
+    private val client by nopClientOption()
 
-    private val name by nameOption("Container name").required()
+    private val name by nameOption("Container name to remove").required()
 
     override fun run() {
-        ClientUtil.nopClientFromConfig(config)
-                .transactionBuilder()
-                .proposeRemoveContainerOperation(config.pubkey().key, name)
+        client.transactionBuilder()
+                .proposeRemoveContainerOperation(client.config.pubkey().key, name)
                 .postSyncAwaitConfirmation()
                 .printResult(
                         "Container removal proposed",
