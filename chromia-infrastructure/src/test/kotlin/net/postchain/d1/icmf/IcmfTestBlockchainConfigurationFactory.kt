54846ee7fccead94f6862556e4a8b6ac94def064
Mikael Ståldal <mikael.staldal@chromaway.com>
2022-10-05 11:58:42 +0200

    Fix compilation error

--- chromia-infrastructure/src/test/kotlin/net/postchain/d1/icmf/IcmfTestBlockchainConfigurationFactory.kt
+++ chromia-infrastructure/src/test/kotlin/net/postchain/d1/icmf/IcmfTestBlockchainConfigurationFactory.kt
@@ -1,17 +1,24 @@
 package net.postchain.d1.icmf
 
 import net.postchain.base.configuration.BlockchainConfigurationData
-import net.postchain.core.ExecutionContext
+import net.postchain.core.EContext
+import net.postchain.crypto.CryptoSystem
 import net.postchain.devtools.testinfra.TestBlockchainConfiguration
 import net.postchain.gtx.GTXBlockchainConfiguration
 import net.postchain.gtx.GTXBlockchainConfigurationFactory
 
 open class IcmfTestBlockchainConfigurationFactory : GTXBlockchainConfigurationFactory() {
 
-    override fun makeBlockchainConfiguration(configurationData: Any, eContext: ExecutionContext): GTXBlockchainConfiguration {
+    override fun makeBlockchainConfiguration(
+        configurationData: Any,
+        eContext: EContext,
+        cryptoSystem: CryptoSystem
+    ): GTXBlockchainConfiguration {
+        val configData = configurationData as BlockchainConfigurationData
         return TestBlockchainConfiguration(
-                configurationData as BlockchainConfigurationData,
-                createGtxModule(configurationData.context.blockchainRID, configurationData, eContext)
+            configData,
+            cryptoSystem,
+            createGtxModule(configurationData.context.blockchainRID, configData, eContext)
         )
     }
 }
