9c4077132b777d37b0035fb5acbdebd46931c8a0
Mikael Ståldal <mikael.staldal@chromaway.com>
2022-10-07 10:12:10 +0200

    Fix handling of currentPointer in anchoring

--- chromia-infrastructure/src/main/kotlin/net/postchain/d1/anchor/ClusterAnchorPipe.kt
+++ chromia-infrastructure/src/main/kotlin/net/postchain/d1/anchor/ClusterAnchorPipe.kt
@@ -27,9 +27,9 @@ class ClusterAnchorPipe(
         return withReadConnection(storage, chainID) { eContext ->
             val dba = DatabaseAccess.of(eContext)
 
-            val blockRID = dba.getBlockRID(eContext, currentPointer + 1)
+            val blockRID = dba.getBlockRID(eContext, currentPointer)
             if (blockRID != null) {
-                highestSeen.getAndUpdate { max(it, currentPointer + 1) }
+                highestSeen.getAndUpdate { max(it, currentPointer) }
                 // Get raw data
                 val rawHeader = dba.getBlockHeader(eContext, blockRID)
                 val rawWitness = dba.getWitnessData(eContext, blockRID)
