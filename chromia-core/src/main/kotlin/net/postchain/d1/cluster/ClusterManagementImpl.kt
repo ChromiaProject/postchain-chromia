d0afc773059e0b4277573b7a233f5f7ae0a1f013
Joar Svartholm <joar.svartholm@chromaway.com>
2022-10-11 15:39:39 +0200

    Code generation for cm-api

--- chromia-core/src/main/kotlin/net/postchain/d1/cluster/ClusterManagementImpl.kt
+++ chromia-core/src/main/kotlin/net/postchain/d1/cluster/ClusterManagementImpl.kt
@@ -1,48 +1,25 @@
 package net.postchain.d1.cluster
 
+import net.postchain.chain0.common.queries.*
+import net.postchain.cm.ClusterManagementClient
 import net.postchain.common.BlockchainRid
 import net.postchain.crypto.PubKey
 import net.postchain.gtv.Gtv
-import net.postchain.gtv.GtvFactory.gtv
 import net.postchain.gtv.mapper.toObject
 
-class ClusterManagementImpl(private val query: (String, Gtv) -> Gtv) : ClusterManagement {
+class ClusterManagementImpl(query: (String, Gtv) -> Gtv) : ClusterManagement {
+    private val cmApi = ClusterManagementClient(query)
 
-    override fun getClusterNames(): Collection<String> =
-            query("cm_get_cluster_names", gtv(mapOf())).asArray().map { it.asString() }
+    override fun getClusterNames(): Collection<String> = cmApi.cmGetClusterNames()
 
-    override fun getClusterInfo(clusterName: String): D1ClusterInfo =
-            query("cm_get_cluster_info", gtv(mapOf("name" to gtv(clusterName)))).toObject()
+    override fun getClusterInfo(clusterName: String): D1ClusterInfo = cmApi.cmGetClusterInfo(clusterName)
+            .let { D1ClusterInfo(it.name, BlockchainRid(it.anchoringChain), it.peers.map { peer -> peer.toObject() }) }
 
-    override fun getBlockchainApiUrls(blockchainRid: BlockchainRid): Collection<String> =
-            query("get_blockchain_api_urls", gtv(mapOf("blockchain_rid" to gtv(blockchainRid)))).asArray()
-                    .map { it.asString() }
+    override fun getBlockchainApiUrls(blockchainRid: BlockchainRid): Collection<String> = cmApi.getBlockchainApiUrls(blockchainRid.data)
 
-    override fun getBlockchainPeers(blockchainRid: BlockchainRid, height: Long): Collection<PubKey> =
-            query(
-                    "cm_get_peer_info", gtv(
-                    mapOf(
-                            "brid" to gtv(blockchainRid),
-                            "height" to gtv(height)
-                    )
-            )
-            ).asArray().map { PubKey(it.asByteArray()) }
+    override fun getBlockchainPeers(blockchainRid: BlockchainRid, height: Long): Collection<PubKey> = cmApi.cmGetPeerInfo(blockchainRid.data, height).map { PubKey(it) }
 
-    override fun getActiveBlockchains(clusterName: String): Collection<BlockchainRid> =
-            query(
-                    "cm_get_cluster_blockchains", gtv(
-                    mapOf(
-                            "name" to gtv(clusterName)
-                    )
-            )
-            ).asArray().map { BlockchainRid(it.asByteArray()) }
+    override fun getActiveBlockchains(clusterName: String): Collection<BlockchainRid> = cmApi.cmGetClusterBlockchains(clusterName).map { BlockchainRid(it) }
 
-    override fun getClusterOfBlockchain(blockchainRid: BlockchainRid): String =
-            query(
-                    "cm_get_blockchain_cluster", gtv(
-                    mapOf(
-                            "brid" to gtv(blockchainRid)
-                    )
-            )
-            ).asString()
+    override fun getClusterOfBlockchain(blockchainRid: BlockchainRid): String = cmApi.cmGetBlockchainCluster(blockchainRid.data)
 }
