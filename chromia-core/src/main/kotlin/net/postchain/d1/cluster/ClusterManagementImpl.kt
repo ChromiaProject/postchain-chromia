4009d8265ca485197d4517969c37e13634b37ef7
Mikael Ståldal <mikael.staldal@chromaway.com>
2022-10-12 10:09:27 +0200

    Use structs in Rell

--- chromia-core/src/main/kotlin/net/postchain/d1/cluster/ClusterManagementImpl.kt
+++ chromia-core/src/main/kotlin/net/postchain/d1/cluster/ClusterManagementImpl.kt
@@ -5,7 +5,6 @@ import net.postchain.cm.ClusterManagementClient
 import net.postchain.common.BlockchainRid
 import net.postchain.crypto.PubKey
 import net.postchain.gtv.Gtv
-import net.postchain.gtv.mapper.toObject
 
 class ClusterManagementImpl(query: (String, Gtv) -> Gtv) : ClusterManagement {
     private val cmApi = ClusterManagementClient(query)
@@ -13,13 +12,22 @@ class ClusterManagementImpl(query: (String, Gtv) -> Gtv) : ClusterManagement {
     override fun getClusterNames(): Collection<String> = cmApi.cmGetClusterNames()
 
     override fun getClusterInfo(clusterName: String): D1ClusterInfo = cmApi.cmGetClusterInfo(clusterName)
-            .let { D1ClusterInfo(it.name, BlockchainRid(it.anchoringChain), it.peers.map { peer -> peer.toObject() }) }
+        .let {
+            D1ClusterInfo(
+                it.name,
+                BlockchainRid(it.anchoringChain),
+                it.peers.map { peer -> D1PeerInfo(peer.apiUrl, PubKey(peer.pubkey)) })
+        }
 
-    override fun getBlockchainApiUrls(blockchainRid: BlockchainRid): Collection<String> = cmApi.getBlockchainApiUrls(blockchainRid.data)
+    override fun getBlockchainApiUrls(blockchainRid: BlockchainRid): Collection<String> =
+        cmApi.getBlockchainApiUrls(blockchainRid.data)
 
-    override fun getBlockchainPeers(blockchainRid: BlockchainRid, height: Long): Collection<PubKey> = cmApi.cmGetPeerInfo(blockchainRid.data, height).map { PubKey(it) }
+    override fun getBlockchainPeers(blockchainRid: BlockchainRid, height: Long): Collection<PubKey> =
+        cmApi.cmGetPeerInfo(blockchainRid.data, height).map { PubKey(it) }
 
-    override fun getActiveBlockchains(clusterName: String): Collection<BlockchainRid> = cmApi.cmGetClusterBlockchains(clusterName).map { BlockchainRid(it) }
+    override fun getActiveBlockchains(clusterName: String): Collection<BlockchainRid> =
+        cmApi.cmGetClusterBlockchains(clusterName).map { BlockchainRid(it) }
 
-    override fun getClusterOfBlockchain(blockchainRid: BlockchainRid): String = cmApi.cmGetBlockchainCluster(blockchainRid.data)
+    override fun getClusterOfBlockchain(blockchainRid: BlockchainRid): String =
+        cmApi.cmGetBlockchainCluster(blockchainRid.data)
 }
