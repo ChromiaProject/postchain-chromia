592558d1ebf726031ff332cdf3a7b8526b7f5213
Joar Svartholm <joar.svartholm@chromaway.com>
2022-11-04 09:26:09 +0100

    Bundle deploy tool with d1-example. Add compile command

--- directory1-example/scripts/setup.sh
+++ directory1-example/scripts/setup.sh
@@ -5,10 +5,12 @@ set -eu
 D=$(dirname "${BASH_SOURCE[0]}")
 C0_SOURCES=$(find "$D"/.. -maxdepth 1 -regex '.*directory1-[0-9]*.[0-9]*.[0-9]*.*-sources.tar.gz')
 PMC=$(find "$D"/.. -maxdepth 1 -regex '.*pmc-directory-[0-9]*.[0-9]*.[0-9]*.*-dist.tar.gz')
+DEPLOY=$(find "$D"/.. -maxdepth 1 -regex '.*chromia-deployment-tool-cli-[0-9]*.[0-9]*.[0-9]*.*-dist.tar.gz')
 tar xf "$C0_SOURCES"
 tar xf "$PMC"
-bash "$POSTCHAIN_DIR"/multigen.sh --source-dir "$D/../directory1/rell" --output-dir "$D/../out" config/run.xml
-bash "$POSTCHAIN_DIR"/multigen.sh --source-dir "$D/../app/src" --output-dir "$D/../app-out" "$D/../app/config/run.xml"
+tar xf "$DEPLOY"
+bash "$D"/chromia-deploy compile --source-dir "$D/../directory1/rell" --output-dir "$D/../out" config/c0-deploy.xml
+bash "$D"/chromia-deploy compile --source-dir "$D/../app/src" --output-dir "$D/../app-out" "$D/../app/config/deploy.xml"
 
 BRID=$(cat "out/blockchains/0/brid.txt")
 echo "brid=$BRID" >> "$D/../provider/alpha/.pmc/config"
