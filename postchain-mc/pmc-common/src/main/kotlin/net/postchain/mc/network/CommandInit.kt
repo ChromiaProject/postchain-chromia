2a53414c6a483aa5144c8aa4b13abcd672d45a64
Joar Svartholm <joar.svartholm@chromaway.com>
2022-11-15 16:54:40 +0100

    Use nullable byte_array as input of anchoring config

--- postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/network/CommandInit.kt
+++ postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/network/CommandInit.kt
@@ -2,7 +2,6 @@ package net.postchain.mc.network
 
 import com.github.ajalt.clikt.core.CliktCommand
 import net.postchain.chain0.common.init.initOperation
-import net.postchain.gtv.GtvNull
 import net.postchain.mc.cli.base.printResult
 import net.postchain.mc.cli.util.nopClientOption
 
@@ -15,7 +14,7 @@ class CommandInit : CliktCommand(
 
     override fun run() {
         client.transactionBuilder()
-                .addOperation("init", GtvNull) // TODO use code generation when it is fixed
+                .initOperation(null)
                 .postSyncAwaitConfirmation()
                 .printResult(
                         "Network was initiated",
