5560623aef93ec5ae9c84d62d40f47d309d6d88c
Eugene Tykulov <eugene.tykulov@chromaway.com>
2022-11-17 17:22:12 +0200

    Code cleanup: rell: .to_hex() removed; CLI: list-blockchain-signers/replicas() refactored

--- postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/blockchain/CommandListBlockchainReplicas.kt
+++ postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/blockchain/CommandListBlockchainReplicas.kt
@@ -1,15 +1,11 @@
 package net.postchain.mc.cli.blockchain
 
 import com.github.ajalt.clikt.core.CliktCommand
-import de.m3y.kformat.Table
-import de.m3y.kformat.table
 import net.postchain.cli.util.blockchainRidOption
-import net.postchain.common.toHex
 import net.postchain.mc.cli.common0.CliExecution
 import net.postchain.mc.cli.includeInactiveOption
+import net.postchain.mc.cli.util.NodeListFormatter
 import net.postchain.mc.cli.util.configOption
-import java.time.Instant
-import java.util.Date
 
 class CommandListBlockchainReplicas : CliktCommand(
         name = "replicas",
@@ -23,20 +19,6 @@ class CommandListBlockchainReplicas : CliktCommand(
 
     override fun run() {
         val list = CliExecution(config).listBlockchainReplicas(blockchainRID.toHex())
-        table {
-            header("pubkey", "host", "port", "active", "last updated")
-            list.forEach {
-                if (includeInactive || it[3].asBoolean()) {
-                    row(
-                            it[0].asByteArray().toHex(),
-                            it[1].asString(),
-                            it[2].asInteger().toString(),
-                            it[3].asBoolean().toString(),
-                            Date.from(Instant.ofEpochMilli(it[4].asInteger()))
-                    )
-                }
-            }
-            hints { borderStyle = Table.BorderStyle.SINGLE_LINE }
-        }.render().also { println(it) }
+        println(NodeListFormatter.render(list, includeInactive))
     }
 }
--- postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/blockchain/CommandListBlockchainSigners.kt
+++ postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/blockchain/CommandListBlockchainSigners.kt
@@ -1,19 +1,15 @@
 package net.postchain.mc.cli.blockchain
 
 import com.github.ajalt.clikt.core.CliktCommand
-import de.m3y.kformat.Table
-import de.m3y.kformat.table
 import net.postchain.cli.util.blockchainRidOption
-import net.postchain.common.toHex
 import net.postchain.mc.cli.common0.CliExecution
 import net.postchain.mc.cli.includeInactiveOption
+import net.postchain.mc.cli.util.NodeListFormatter
 import net.postchain.mc.cli.util.configOption
-import java.time.Instant
-import java.util.*
 
 class CommandListBlockchainSigners : CliktCommand(
-    name = "signers",
-    help = "List blockchain signers"
+        name = "signers",
+        help = "List blockchain signers"
 ) {
     private val config by configOption()
 
@@ -22,22 +18,7 @@ class CommandListBlockchainSigners : CliktCommand(
     private val includeInactive by includeInactiveOption()
 
     override fun run() {
-        val listSigners = CliExecution(config).listBlockchainSigners(blockchainRID.toHex())
-
-        table {
-            header("pubkey", "host", "port", "active", "last updated")
-            listSigners.forEach {
-                if (includeInactive || it[3].asBoolean()) {
-                    row(
-                            it[0].asByteArray().toHex(),
-                            it[1].asString(),
-                            it[2].asInteger().toString(),
-                            it[3].asBoolean().toString(),
-                            Date.from(Instant.ofEpochMilli(it[4].asInteger()))
-                    )
-                }
-            }
-            hints { borderStyle = Table.BorderStyle.SINGLE_LINE }
-        }.render().also { println(it) }
+        val list = CliExecution(config).listBlockchainSigners(blockchainRID.toHex())
+        println(NodeListFormatter.render(list, includeInactive))
     }
 }
