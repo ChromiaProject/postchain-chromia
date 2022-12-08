7803f7c25cf2ed9b75d80b421a9097e1f862e859
Joar Svartholm <joar.svartholm@chromaway.com>
2022-12-08 16:02:53 +0100

    Remove deployment-tool module

--- chromia-deployment-tool/src/main/kotlin/net/postchain/deployment/ParsedConfiguration.kt
+++
@@ -1,9 +0,0 @@
-package net.postchain.deployment
-
-import net.postchain.common.BlockchainRid
-
-data class ParsedConfiguration(
-        val runXml: String,
-        val blockchainRid: BlockchainRid?,
-        val containerName: String?
-)
