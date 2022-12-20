13b513d2ddd2b3721ef2f499cc60d21d70475ac2
Eugene Tykulov <eugene.tykulov@chromaway.com>
2022-12-20 21:36:20 +0200

    Provider quotas CLI: `pmc provider quota` / CommandProposeProviderQuota added

--- postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/provider/CommonProviderCommand.kt
+++ postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/provider/CommonProviderCommand.kt
@@ -13,6 +13,7 @@ class CommonProviderCommand : CliktCommand(
 fun providerCommands() = CommonProviderCommand().subcommands(
         CommandGetProviderInfo(),
         CommandListProviderQuotas(),
+        CommandProposeProviderQuota(),
         CommandListProviderNodes(),
         CommandListProviders(),
         CommandRegisterProvider(),
