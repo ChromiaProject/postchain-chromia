89b6eea36eef881ee69f2fbb6f2dea0af155dd10
Mikael Ståldal <mikael.staldal@chromaway.com>
2022-12-21 10:11:33 +0000

    Move test stuff from postchain-distribution repo

---
+++ deployment-test/src/test/kotlin/net/postchain/dapp/extensions.kt
@@ -0,0 +1,42 @@
+package net.postchain.dapp
+
+import net.postchain.client.core.TransactionResult
+import net.postchain.client.transaction.Postable
+import net.postchain.common.tx.TransactionStatus
+import java.lang.Thread.sleep
+import java.time.Duration
+
+/**
+ * When we call the "client" it will try 20 times to get the status for the TX, so
+ * we don't have to do much ourselves here.
+ *
+ * We will repeat the TX ONLY if we get REJECTED, b/c that's a serious problem
+ *
+ * (WAITING is considered successful, but it could mean that something is wrong since the
+ * client tried 20 times and still didn't get "CONFIRMED".)
+ *
+ */
+fun Postable.postTransactionUntilConfirmed(
+    transactionName: String = "",
+    retries: Int = 20,
+    timeOut: Duration = Duration.ofSeconds(2)
+): TransactionResult {
+    repeat(retries) { attempt ->
+        val txRes = postAwaitConfirmation()
+        if (txRes.status == TransactionStatus.REJECTED && isRetryableStatus(txRes.httpStatusCode) ||
+            txRes.status == TransactionStatus.UNKNOWN
+        ) {
+            println("  Tx $transactionName result (attempt ${attempt}) was: ${txRes.status}, code: ${txRes.httpStatusCode}")
+            sleep(timeOut.toMillis())
+        } else {
+            println("  TX $transactionName result was: ${txRes.status} , code: ${txRes.httpStatusCode} ${txRes.rejectReason?.let { ", reason: $it" } ?: ""}")
+            return txRes
+        }
+    }
+    throw RuntimeException("Transaction $transactionName failed")
+}
+
+private fun isRetryableStatus(status: Int?) = when (status) {
+    404, 503, 500 -> true
+    else -> false
+}
---
+++ deployment-test/src/test/kotlin/net/postchain/postgres/ChromaWayPostgresContainer.kt
@@ -0,0 +1,46 @@
+package net.postchain.postgres
+
+import org.testcontainers.containers.JdbcDatabaseContainer
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy
+import org.testcontainers.utility.DockerImageName
+import java.time.Duration
+import java.time.temporal.ChronoUnit
+
+class ChromaWayPostgresContainer(dockerImageName: DockerImageName = DockerImageName.parse("postgres:14.1-alpine")) : JdbcDatabaseContainer<ChromaWayPostgresContainer>(dockerImageName) {
+
+    companion object {
+        const val POSTGRESQL_PORT = 5432
+    }
+
+    init {
+        waitStrategy = LogMessageWaitStrategy()
+                .withRegEx(".*database system is ready to accept connections.*\\s")
+                .withTimes(2)
+                .withStartupTimeout(Duration.of(60L, ChronoUnit.SECONDS))
+        setCommand("postgres", "-c", "fsync=off")
+        addExposedPort(POSTGRESQL_PORT)
+        addEnv("POSTGRES_PASSWORD", "postchain")
+    }
+
+    override fun getUsername() = "postchain"
+    override fun getPassword() = "postchain"
+    override fun getDatabaseName() = "postchain"
+    override fun getTestQueryString() = "SELECT 1;"
+    override fun getDriverClassName() = "org.postgresql.Driver"
+
+    override fun getJdbcUrl(): String {
+        return databaseUrl("$host:${getMappedPort(POSTGRESQL_PORT)}")
+    }
+
+    fun networkJdbcUrl(): String = network?.let { databaseUrl(networkAliases.first()) } ?: jdbcUrl
+
+    private fun databaseUrl(ip: String): String {
+        val additionalUrlParams = constructUrlParameters("?", "&")
+        return "jdbc:postgresql://$ip/$databaseName$additionalUrlParams"
+    }
+
+    fun createChainDatabaseCommunicator(chainId: Long, schema: String): ChainDatabaseCommunicator {
+        val dbConfig = DatabaseConfig(driverClassName, jdbcUrl, username, password)
+        return ChainDatabaseCommunicator(chainId, schema, dbConfig)
+    }
+}
