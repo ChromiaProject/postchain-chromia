7e991ded5d1ba369c45ea8e86d18258088aedc5c
Eugene Tykulov <eugene.tykulov@chromaway.com>
2023-01-10 11:05:42 +0200

    CLI: `pmc node remove` added

--- postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/node/NodeCommand.kt
+++ postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/node/NodeCommand.kt
@@ -14,6 +14,7 @@ fun nodeCommands() = NodeCommand().subcommands(
         CommandReplaceNode(),
         CommandDisableNode(),
         CommandEnableNode(),
+        CommandRemoveNode(),
         CommandGetNodeInfo(),
         CommandGetNodeListVersion(),
         CommandListBlockchainsForNode(),
