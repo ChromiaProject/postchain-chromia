c48488c2b6425277fd8822027adcbcd14f14d0fa
Eugene Tykulov <eugene.tykulov@chromaway.com>
2023-01-19 14:10:10 +0200

    POS-595: D1: Version.initialVersion const added

--- postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/network/Version.kt
+++ postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/network/Version.kt
@@ -5,11 +5,15 @@ import net.postchain.chain0.common.directoryVersion
 
 class Version(private val client: net.postchain.client.core.PostchainClient) {
 
+    companion object {
+        val initialVersion = net.postchain.chain0.common.Version(Codename.Delta, "0.1.0")
+    }
+
     val version by lazy {
         try {
             client.directoryVersion()
         } catch (_: Throwable) {
-            net.postchain.chain0.common.Version(Codename.Delta, "0.1.0")
+            initialVersion
         }
     }
 }
