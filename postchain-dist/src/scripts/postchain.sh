f59b803c87cef115585adcd27a9e9e59f69bb702
Robert Wideberg <robert.wideberg@chromaway.com>
2023-02-01 14:03:50 +0100

    Add flag to dist scripts

--- postchain-dist/src/scripts/postchain.sh
+++ postchain-dist/src/scripts/postchain.sh
@@ -4,4 +4,4 @@ D=$(dirname "${BASH_SOURCE[0]}")
 JAR_FILE=$(find $D/lib -maxdepth 1 -regex '.*postchain-cli-[0-9]*.[0-9]*.[0-9]*.*.jar')
 RELL_FILE=$(find $D/lib -maxdepth 1 -regex '.*rell-[0-9]*.[0-9]*.[0-9]*.*.jar')
 
-exec "${RELL_JAVA:-java}" -classpath "$JAR_FILE:$RELL_FILE:$D/lib/deps/*" net.postchain.AppKt "$@"
+exec "${RELL_JAVA:-java}" -XX:+CrashOnOutOfMemoryError -classpath "$JAR_FILE:$RELL_FILE:$D/lib/deps/*" net.postchain.AppKt "$@"
