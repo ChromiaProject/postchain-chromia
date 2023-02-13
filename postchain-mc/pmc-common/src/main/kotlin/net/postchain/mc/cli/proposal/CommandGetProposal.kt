113cd60c0087876cba4a63133199c92d2155996d
Robert Wideberg <robert.wideberg@chromaway.com>
2023-02-13 17:05:39 +0100

    Correct units

--- postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/proposal/CommandGetProposal.kt
+++ postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/proposal/CommandGetProposal.kt
@@ -157,8 +157,8 @@ class CommandGetProposal : CliktCommand(
                     row("CPU:", pcl.cpu.toString())
                     row("RAM (MB):", pcl.ram.toString())
                     row("Storage (MB):", pcl.storage.toString())
-                    row("Disk I/O read (MB):", pcl.ioRead.toString())
-                    row("Disk I/O write (MB):", pcl.ioWrite.toString())
+                    row("Disk I/O read (MiB/s):", pcl.ioRead.toString())
+                    row("Disk I/O write (MiB/s):", pcl.ioWrite.toString())
                     hints { defaultAlignment = Table.Hints.Alignment.LEFT }
                 }.render().toString()
             }
@@ -171,8 +171,8 @@ class CommandGetProposal : CliktCommand(
                     row("Default container CPU:", pcl.defaultContainerCpu.toString())
                     row("Default container RAM (MB):", pcl.defaultContainerRam.toString())
                     row("Default container storage (MB):", pcl.defaultContainerStorage.toString())
-                    row("Default disk I/O read (MB):", pcl.defaultContainerIoRead.toString())
-                    row("Default disk I/O write (MB):", pcl.defaultContainerIoWrite.toString())
+                    row("Default disk I/O read (MiB/s):", pcl.defaultContainerIoRead.toString())
+                    row("Default disk I/O write (MiB/s):", pcl.defaultContainerIoWrite.toString())
                     hints { defaultAlignment = Table.Hints.Alignment.LEFT }
                 }.render().toString()
             }
