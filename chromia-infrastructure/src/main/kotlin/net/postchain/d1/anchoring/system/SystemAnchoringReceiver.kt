ad83c8365906c56824c9cfe7dbe1c5569e788408
Eugene Tykulov <eugene.tykulov@chromaway.com>
2023-03-07 13:08:07 +0200

    ICMF: ConcurrentHashMap in anchoring receivers

--- chromia-infrastructure/src/main/kotlin/net/postchain/d1/anchoring/system/SystemAnchoringReceiver.kt
+++ chromia-infrastructure/src/main/kotlin/net/postchain/d1/anchoring/system/SystemAnchoringReceiver.kt
@@ -4,9 +4,10 @@ import net.postchain.common.BlockchainRid
 import net.postchain.d1.anchoring.AnchoringPipe
 import net.postchain.d1.anchoring.AnchoringReceiver
 import net.postchain.d1.cluster.ClusterManagement
+import java.util.concurrent.ConcurrentHashMap
 
 class SystemAnchoringReceiver(private val clusterManagement: ClusterManagement) : AnchoringReceiver {
-    override val localPipes = mutableMapOf<Long, AnchoringPipe>()
+    override val localPipes = ConcurrentHashMap<Long, AnchoringPipe>()
 
     override fun getRelevantPipes(): List<AnchoringPipe> {
         return localPipes.values.filter { it.blockchainRid in getRelevantChains() }
