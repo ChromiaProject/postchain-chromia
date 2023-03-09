7c1463ef0c2e1d279c0a5d01f56e46b8dff74361
Eugene Tykulov <eugene.tykulov@chromaway.com>
2023-03-09 13:10:28 +0200

    Review comments

--- postchain-mc/pmc-common/src/test/kotlin/net/postchain/mc/test/RellIntegrationTest.kt
+++ postchain-mc/pmc-common/src/test/kotlin/net/postchain/mc/test/RellIntegrationTest.kt
@@ -6,13 +6,13 @@ import net.postchain.client.config.PostchainClientConfig
 import net.postchain.client.request.EndpointPool
 import net.postchain.concurrent.util.get
 import net.postchain.core.*
-import net.postchain.devtools.IntegrationTestSetup
 import net.postchain.core.block.BlockBuilder
 import net.postchain.core.block.BlockBuildingStrategy
 import net.postchain.core.block.BlockData
 import net.postchain.core.block.BlockQueries
 import net.postchain.crypto.KeyPair
 import net.postchain.crypto.devtools.KeyPairHelper
+import net.postchain.devtools.IntegrationTestSetup
 import net.postchain.devtools.utils.configuration.BlockchainSetup
 import net.postchain.devtools.utils.configuration.BlockchainSetupFactory
 import net.postchain.devtools.utils.configuration.system.SystemSetupFactory
@@ -73,11 +73,11 @@ abstract class RellIntegrationTest : IntegrationTestSetup() {
 
     protected fun cliConf(keyIndex: Int): PostchainClientConfig {
         return PostchainClientConfig(
-            nodes[0].getBlockchainRid(0)!!,
-            EndpointPool.singleUrl("http://127.0.0.1:" + nodes[0].getRestApiHttpPort()),
-            listOf(
-                KeyPair.of(KeyPairHelper.pubKeyHex(keyIndex), KeyPairHelper.privKeyHex(keyIndex))
-            )
+                nodes[0].getBlockchainRid(0)!!,
+                EndpointPool.singleUrl("http://127.0.0.1:" + nodes[0].getRestApiHttpPort()),
+                listOf(
+                        KeyPair.of(KeyPairHelper.pubKeyHex(keyIndex), KeyPairHelper.privKeyHex(keyIndex))
+                )
         )
     }
 
@@ -128,9 +128,9 @@ abstract class RellIntegrationTest : IntegrationTestSetup() {
 
 @Suppress("UNUSED_PARAMETER")
 class SmartOnDemandBlockBuildingStrategy(
-    configData: BaseBlockBuildingStrategyConfigurationData,
-    blockQueries: BlockQueries,
-    val txQueue: TransactionQueue
+        configData: BaseBlockBuildingStrategyConfigurationData,
+        blockQueries: BlockQueries,
+        val txQueue: TransactionQueue
 ) : BlockBuildingStrategy {
 
     companion object : KLogging()
@@ -156,7 +156,6 @@ class SmartOnDemandBlockBuildingStrategy(
     }
 
     override fun blockFailed() {
-        TODO("Not yet implemented")
     }
 
     fun awaitCommitted(height: Int) {
