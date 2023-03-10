17e3b0eb3d540c1d6f673d03f1a9c9c685ae72d1
Robert Wideberg <robert.wideberg@chromaway.com>
2023-03-10 15:12:11 +0100

    Wrap unsafe block height query

--- deployment-test/src/test/kotlin/net/postchain/images/directory1/Directory1DeploymentBase.kt
+++ deployment-test/src/test/kotlin/net/postchain/images/directory1/Directory1DeploymentBase.kt
@@ -404,7 +404,7 @@ abstract class Directory1DeploymentBase {
 
         fun getAssertingParam(): Long {
             val brid = dapps["test-dapp2"]!!
-            val height = node1.client(brid).currentBlockHeight()
+            val height = awaitQueryResult { node1.client(brid).currentBlockHeight() }!!
             val config0 = node1.c0.nmGetBlockchainConfiguration(dapps["test-dapp2"]!!, height)!!
             return GtvDecoder.decodeGtv(config0).asDict()["blockstrategy"]!!["maxblocktransactions"]!!.asInteger()
         }
