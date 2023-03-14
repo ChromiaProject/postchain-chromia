ae12634296c7b421175c7f6f238154659f135a17
Mikael Ögren <mikael.ogren@chromaway.com>
2023-03-14 15:19:56 +0100

    Added possibility to override log conf file.

--- chromia-dist/src/scripts/chromia.sh
+++ chromia-dist/src/scripts/chromia.sh
@@ -2,6 +2,6 @@
 set -eu
 D=$(dirname "${BASH_SOURCE[0]}")
 
-JVM_FLAGS="-XX:+CrashOnOutOfMemoryError -Dlog4j2.configurationFile=$D/log4j2.yml"
+JVM_FLAGS="-XX:+CrashOnOutOfMemoryError -Dlog4j2.configurationFile=${LOG4J_CONFIGURATION_FILE:=$D/log4j2.yml}"
 
 exec "${RELL_JAVA:-java}" $JVM_FLAGS -classpath "$D/lib/*" net.postchain.AppKt "$@"
