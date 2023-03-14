2e10357f701ff7033651c6e5672054c4116d3eb4
Mikael Ögren <mikael.ogren@chromaway.com>
2023-03-14 14:53:30 +0100

    POS-704: Removed logging to file in docker containers. Added default logging with deletion to chromia-dist. Updated providers documentation with logging.

--- chromia-dist/src/scripts/chromia.sh
+++ chromia-dist/src/scripts/chromia.sh
@@ -2,6 +2,6 @@
 set -eu
 D=$(dirname "${BASH_SOURCE[0]}")
 
-JVM_FLAGS="-XX:+CrashOnOutOfMemoryError"
+JVM_FLAGS="-XX:+CrashOnOutOfMemoryError -Dlog4j2.configurationFile=$D/log4j2.yml"
 
 exec "${RELL_JAVA:-java}" $JVM_FLAGS -classpath "$D/lib/*" net.postchain.AppKt "$@"
