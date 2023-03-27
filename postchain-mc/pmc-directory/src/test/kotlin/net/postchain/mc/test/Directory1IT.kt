de7fe5719c55103191d4aa2cb83c5c54cc58c9c5
Mikael Ögren <mikael.ogren@chromaway.com>
2023-03-27 11:58:34 +0000

    Refactor cluster proposal

--- postchain-mc/pmc-directory/src/test/kotlin/net/postchain/mc/test/Directory1IT.kt
+++ postchain-mc/pmc-directory/src/test/kotlin/net/postchain/mc/test/Directory1IT.kt
@@ -29,12 +29,12 @@ import net.postchain.chain0.nm_api.nmGetPeerListVersion
 import net.postchain.chain0.proposal.ProposalType
 import net.postchain.chain0.proposal.getProposal
 import net.postchain.chain0.proposal.getProposalsSince
-import net.postchain.chain0.proposal.proposeClusterProviderOperation
-import net.postchain.chain0.proposal_provider.proposeProviderIsSystemOperation
-import net.postchain.chain0.proposal_provider.proposeProviderStateOperation
 import net.postchain.chain0.proposal_blockchain.proposeBlockchainActionOperation
 import net.postchain.chain0.proposal_blockchain.proposeBlockchainOperation
 import net.postchain.chain0.proposal_blockchain.proposeConfigurationAtOperation
+import net.postchain.chain0.proposal_cluster.proposeClusterProviderOperation
+import net.postchain.chain0.proposal_provider.proposeProviderIsSystemOperation
+import net.postchain.chain0.proposal_provider.proposeProviderStateOperation
 import net.postchain.chain0.proposal_voter_set.proposeUpdateVoterSetOperation
 import net.postchain.client.config.PostchainClientConfig
 import net.postchain.client.core.PostchainClient
