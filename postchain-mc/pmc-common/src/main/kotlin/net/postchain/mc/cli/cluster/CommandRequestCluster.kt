e8b1b67687120843754b6177f927ef1918cd73b0
Mikael Ögren <mikael.ogren@chromaway.com>
2023-03-28 08:18:00 +0000

    Refactor direct operations

--- postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/cluster/CommandRequestCluster.kt
+++ postchain-mc/pmc-common/src/main/kotlin/net/postchain/mc/cli/cluster/CommandRequestCluster.kt
@@ -5,7 +5,7 @@ import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.long
-import net.postchain.chain0.cluster.cluster_op.requestClusterOperation
+import net.postchain.chain0.direct_cluster.requestClusterOperation
 import net.postchain.mc.cli.base.printResult
 import net.postchain.mc.cli.base.pubkey
 import net.postchain.mc.cli.util.nameOrGenerateOption
