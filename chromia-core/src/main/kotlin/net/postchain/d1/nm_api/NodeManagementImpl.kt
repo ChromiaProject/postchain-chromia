cda2016c1a41a06681afe22655728eee40c87fc3
Eugene Tykulov <eugene.tykulov@chromaway.com>
2023-04-19 13:24:41 +0300

    PCU Code cleanup: NM.getPendingBlockchainConfigs() removed

--- chromia-core/src/main/kotlin/net/postchain/d1/nm_api/NodeManagementImpl.kt
+++ chromia-core/src/main/kotlin/net/postchain/d1/nm_api/NodeManagementImpl.kt
@@ -2,18 +2,12 @@ package net.postchain.d1.nm_api
 
 import net.postchain.chromia.nm_api.NmGetBlockchainConfigurationV5Result
 import net.postchain.chromia.nm_api.NmGetPendingBlockchainConfigurationByHashResult
-import net.postchain.chromia.nm_api.NmGetPendingBlockchainConfigurationResult
 import net.postchain.chromia.nm_api.nmGetBlockchainConfigurationV5
-import net.postchain.chromia.nm_api.nmGetPendingBlockchainConfiguration
 import net.postchain.chromia.nm_api.nmGetPendingBlockchainConfigurationByHash
 import net.postchain.client.core.PostchainQuery
 import net.postchain.common.BlockchainRid
 
 class NodeManagementImpl(private val query: PostchainQuery) : NodeManagement {
-
-    override fun getPendingBlockchainConfigs(blockchainRid: BlockchainRid, height: Long): List<NmGetPendingBlockchainConfigurationResult> {
-        return query.nmGetPendingBlockchainConfiguration(blockchainRid, height)
-    }
 
     override fun getPendingBlockchainConfigByHash(blockchainRid: BlockchainRid, configHash: ByteArray): NmGetPendingBlockchainConfigurationByHashResult? {
         return query.nmGetPendingBlockchainConfigurationByHash(blockchainRid, configHash)
