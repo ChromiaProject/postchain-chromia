46b07e115771083d20c8df0dba94ea2cd62c1ef7
Eugene Tykulov <eugene.tykulov@chromaway.com>
2023-04-20 21:03:19 +0300

    PCU has been activated for all Deployment tests

--- deployment-test/src/test/kotlin/net/postchain/images/directory1/Directory1DeploymentAllSubnodesNightly.kt
+++ deployment-test/src/test/kotlin/net/postchain/images/directory1/Directory1DeploymentAllSubnodesNightly.kt
@@ -20,6 +20,7 @@ class Directory1DeploymentAllSubnodesNightly : Directory1DeploymentBase() {
                             PostchainContainer.MOUNT_DIR, BindMode.READ_ONLY
                     )
                     .withEnv("POSTCHAIN_CONFIG", "${PostchainContainer.MOUNT_DIR}/node-config.properties")
+                    .withEnv("POSTCHAIN_PCU", true.toString())
             node2 = postchainServer("node2", Slf4jLogConsumer(node2Logger.underlyingLogger, true),
                     KeyPair.of("03F9ABC05F7D7639AEC97B18784D5C83CA82D1EAF8F96DC31E77A83F21DDE67F95", "FFC28105CFE2CC336624DCDFDEDB58157B37ED565C29F11A3B54B8F721DBA7C5"),
                     "config-all-subnodes")
@@ -31,6 +32,7 @@ class Directory1DeploymentAllSubnodesNightly : Directory1DeploymentBase() {
                             PostchainContainer.MOUNT_DIR, BindMode.READ_ONLY
                     )
                     .withEnv("POSTCHAIN_CONFIG", "${PostchainContainer.MOUNT_DIR}/node-config.properties")
+                    .withEnv("POSTCHAIN_PCU", true.toString())
             node3 = postchainServer("node3", Slf4jLogConsumer(node3Logger.underlyingLogger, true),
                     KeyPair.of("03D01591E5466B07AC1D1F77BEBE2164AB0BA31366FBF005907F28FD144D64B871", "AD329F5C4E4DDF226D1A4948D7A2CCB34E76F64D4972B934FDBBDBEF4CA7B905"),
                     "config-all-subnodes")
@@ -42,6 +44,7 @@ class Directory1DeploymentAllSubnodesNightly : Directory1DeploymentBase() {
                             PostchainContainer.MOUNT_DIR, BindMode.READ_ONLY
                     )
                     .withEnv("POSTCHAIN_CONFIG", "${PostchainContainer.MOUNT_DIR}/node-config.properties")
+                    .withEnv("POSTCHAIN_PCU", true.toString())
 
             removeSubnodeContainers()
             startNodesAndChain0()
--- deployment-test/src/test/kotlin/net/postchain/images/directory1/Directory1DeploymentMixNightly.kt
+++ deployment-test/src/test/kotlin/net/postchain/images/directory1/Directory1DeploymentMixNightly.kt
@@ -12,9 +12,11 @@ class Directory1DeploymentMixNightly : Directory1DeploymentBase() {
             node1 = postchainServer("node1", Slf4jLogConsumer(node1Logger.underlyingLogger, true),
                     KeyPair.of("03ECD350EEBC617CBBFBEF0A1B7AE553A748021FD65C7C50C5ABB4CA16D4EA5B05", "BBBDFE956021912512E14BB081B27A35A0EABC4098CB687E973C434006BCE114"),
                     "config-mix")
+                    .withEnv("POSTCHAIN_PCU", true.toString())
             node2 = postchainServer("node2", Slf4jLogConsumer(node2Logger.underlyingLogger, true),
                     KeyPair.of("03F9ABC05F7D7639AEC97B18784D5C83CA82D1EAF8F96DC31E77A83F21DDE67F95", "FFC28105CFE2CC336624DCDFDEDB58157B37ED565C29F11A3B54B8F721DBA7C5"),
                     "config-mix")
+                    .withEnv("POSTCHAIN_PCU", true.toString())
             node3 = postchainServer("node3", Slf4jLogConsumer(node3Logger.underlyingLogger, true),
                     KeyPair.of("03D01591E5466B07AC1D1F77BEBE2164AB0BA31366FBF005907F28FD144D64B871", "AD329F5C4E4DDF226D1A4948D7A2CCB34E76F64D4972B934FDBBDBEF4CA7B905"),
                     "config-mix")
@@ -26,6 +28,7 @@ class Directory1DeploymentMixNightly : Directory1DeploymentBase() {
                             PostchainContainer.MOUNT_DIR, BindMode.READ_ONLY
                     )
                     .withEnv("POSTCHAIN_CONFIG", "${PostchainContainer.MOUNT_DIR}/node-config.properties")
+                    .withEnv("POSTCHAIN_PCU", true.toString())
 
             removeSubnodeContainers()
             startNodesAndChain0()
