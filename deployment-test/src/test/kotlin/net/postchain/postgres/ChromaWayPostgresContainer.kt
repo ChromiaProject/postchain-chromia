ebbf5a8101e071ada348d71d7b93cd0133480734
Mikael Ögren <mikael.ogren@chromaway.com>
2023-04-20 14:28:36 +0200

    Updated to postgres 14.7

--- deployment-test/src/test/kotlin/net/postchain/postgres/ChromaWayPostgresContainer.kt
+++ deployment-test/src/test/kotlin/net/postchain/postgres/ChromaWayPostgresContainer.kt
@@ -6,7 +6,7 @@ import org.testcontainers.utility.DockerImageName
 import java.time.Duration
 import java.time.temporal.ChronoUnit
 
-class ChromaWayPostgresContainer(dockerImageName: DockerImageName = DockerImageName.parse("postgres:14.1-alpine")) : JdbcDatabaseContainer<ChromaWayPostgresContainer>(dockerImageName) {
+class ChromaWayPostgresContainer(dockerImageName: DockerImageName = DockerImageName.parse("postgres:14.7-alpine3.17")) : JdbcDatabaseContainer<ChromaWayPostgresContainer>(dockerImageName) {
 
     companion object {
         const val POSTGRESQL_PORT = 5432
