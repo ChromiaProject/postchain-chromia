aa616d68bf969a3b3531c3f0b1116943928b66ef
Mikael Ståldal <mikael.staldal@chromaway.com>
2023-05-16 17:56:30 +0200

    Cap size of anchoring transaction
    (cherry picked from commit 77257d84a55c9be3114ee43b89036ef181397dd5)

--- chromia-infrastructure/src/main/kotlin/net/postchain/d1/anchoring/AnchoringProcessManagerExtension.kt
+++ chromia-infrastructure/src/main/kotlin/net/postchain/d1/anchoring/AnchoringProcessManagerExtension.kt
@@ -1,6 +1,8 @@
 package net.postchain.d1.anchoring
 
 import net.postchain.PostchainContext
+import net.postchain.base.BaseBlockBuildingStrategyConfigurationData
+import net.postchain.base.configuration.KEY_BLOCKSTRATEGY
 import net.postchain.base.gtv.BlockHeaderData
 import net.postchain.cm.cm_api.ClusterManagementImpl
 import net.postchain.common.BlockchainRid
@@ -11,6 +13,8 @@ import net.postchain.core.BlockchainProcess
 import net.postchain.core.RemoteBlockchainProcess
 import net.postchain.core.RemoteBlockchainProcessConnectable
 import net.postchain.d1.cluster.ClusterManagement
+import net.postchain.gtv.GtvFactory
+import net.postchain.gtv.mapper.toObject
 import net.postchain.gtx.GTXModule
 import net.postchain.gtx.GTXModuleAware
 import net.postchain.managed.config.ManagedDataSourceAware
@@ -38,6 +42,10 @@ open class AnchoringProcessManagerExtension(
                 it.isSigner = process::isSigner
                 val clusterManagement = createClusterManagement(cfg)
                 it.clusterManagement = clusterManagement
+
+                val blockStrategyConfig = cfg.rawConfig[KEY_BLOCKSTRATEGY] ?: GtvFactory.gtv(mapOf())
+                it.maxBlockSize = blockStrategyConfig.toObject<BaseBlockBuildingStrategyConfigurationData>().maxBlockSize
+
                 it.createReceiver(cfg.blockchainRid)
                 localDispatcher.connectReceiver(cfg.chainID, it.anchoringReceiver)
             }
