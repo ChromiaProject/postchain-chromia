326bf59d39769a57830f5247a1044b3674ca7aee
Eugene Tykulov <eugene.tykulov@chromaway.com>
2023-06-12 17:12:31 +0300

    Bugfix: ManagedBlockchainConfigProvider loads really relevant signers of chain0 (ignores faulty config)

--- chromia-core/src/main/kotlin/net/postchain/d1/nm_api/NodeManagement.kt
+++ chromia-core/src/main/kotlin/net/postchain/d1/nm_api/NodeManagement.kt
@@ -5,6 +5,8 @@ import net.postchain.chromia.nm_api.NmGetPendingBlockchainConfigurationByHashRes
 import net.postchain.common.BlockchainRid
 
 interface NodeManagement {
+    fun nmApiVersion(): Long
+    fun getManagementChain(): BlockchainRid
     fun getPendingBlockchainConfigByHash(blockchainRid: BlockchainRid, configHash: ByteArray): NmGetPendingBlockchainConfigurationByHashResult?
     fun getBlockchainConfiguration(blockchainRid: BlockchainRid, height: Long): NmGetBlockchainConfigurationV5Result?
 }
