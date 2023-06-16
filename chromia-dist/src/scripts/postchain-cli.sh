007529b99a38d0e890fcbdb440cc39a691081fbb
Eugene Tykulov <eugene.tykulov@chromaway.com>
2023-06-16 15:06:32 +0300

    Review fixes: .sh script added

---
+++ chromia-dist/src/scripts/postchain-cli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+D=$(dirname "${BASH_SOURCE[0]}")
+
+export LOG4J_CONFIGURATION_FILE=${LOG4J_CONFIGURATION_FILE:=$D/log4j2.yml}
+
+JVM_FLAGS="-XX:+CrashOnOutOfMemoryError"
+
+exec "${RELL_JAVA:-java}" $JVM_FLAGS -classpath "$D/lib/*" net.postchain.AppKt "$@"
