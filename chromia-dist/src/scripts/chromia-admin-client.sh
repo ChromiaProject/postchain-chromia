53b7c5caa52ebb60be6f451f206a508ab97aacaa
Mikael Ståldal <mikael.staldal@chromaway.com>
2023-06-16 17:06:39 +0200

    Include admin-client in chromia-dist, and rename some scripts

---
+++ chromia-dist/src/scripts/chromia-admin-client.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -eu
+D=$(dirname "${BASH_SOURCE[0]}")
+
+JVM_FLAGS="-XX:+CrashOnOutOfMemoryError"
+
+exec "${RELL_JAVA:-java}" $JVM_FLAGS -classpath "$D/lib/*" net.postchain.admin.cli.AppKt "$@"
