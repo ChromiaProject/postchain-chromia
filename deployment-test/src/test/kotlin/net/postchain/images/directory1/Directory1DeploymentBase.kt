0e5d6b781e8bd0a1d2949fb0c88f5adb825d759a
Eugene Tykulov <eugene.tykulov@chromaway.com>
2023-10-18 12:14:08 +0300

    DeploymentIT: activate `Subnode container have been launched` test only for master-sub test suites

--- deployment-test/src/test/kotlin/net/postchain/images/directory1/Directory1DeploymentBase.kt
+++ deployment-test/src/test/kotlin/net/postchain/images/directory1/Directory1DeploymentBase.kt
@@ -500,18 +500,20 @@ abstract class Directory1DeploymentBase {
     @Test
     @Order(19)
     fun `Subnode container stops if empty`() {
-        testLogger.info("Asserting that the container stops if there are no running blockchains in it")
+        if (numberOfMasterNodes > 0) {
+            testLogger.info("Asserting that the container stops if there are no running blockchains in it")
 
-        // REMOVE test_dapp3
-        node1.c0.transactionBuilder()
-                .proposeBlockchainActionOperation(node1.providerPubkey, dapps["test_dapp3"]!!, BlockchainAction.remove, "")
-                .postTransactionUntilConfirmed("Change state to ${BlockchainState.REMOVED.name} for test_dapp3")
+            // REMOVE test_dapp3
+            node1.c0.transactionBuilder()
+                    .proposeBlockchainActionOperation(node1.providerPubkey, dapps["test_dapp3"]!!, BlockchainAction.remove, "")
+                    .postTransactionUntilConfirmed("Change state to ${BlockchainState.REMOVED.name} for test_dapp3")
 
-        awaitUntilAsserted {
-            val fooDockerContainer = dockerClient.listContainers(DockerClient.ListContainersParam.allContainers()).firstOrNull {
-                it.names().any { name -> name.contains(fooContainer) }
+            awaitUntilAsserted {
+                val fooDockerContainer = dockerClient.listContainers(DockerClient.ListContainersParam.allContainers()).firstOrNull {
+                    it.names().any { name -> name.contains(fooContainer) }
+                }
+                assertThat(fooDockerContainer?.state()).isEqualTo("exited")
             }
-            assertThat(fooDockerContainer?.state()).isEqualTo("exited")
         }
     }
 
