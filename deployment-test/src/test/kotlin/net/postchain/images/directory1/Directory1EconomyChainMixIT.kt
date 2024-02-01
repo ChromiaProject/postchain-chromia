235b93f0b5c379e6a4bdfd2ba17269a6b177311c
Eugene Tykulov <eugene.tykulov@chromaway.com>
2024-02-01 10:10:34 +0200

    EC: blockchain migration added to the `Upgrade container()` integration test

--- deployment-test/src/test/kotlin/net/postchain/images/directory1/Directory1EconomyChainMixIT.kt
+++ deployment-test/src/test/kotlin/net/postchain/images/directory1/Directory1EconomyChainMixIT.kt
@@ -3,12 +3,16 @@ package net.postchain.images.directory1
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isFalse
+import assertk.assertions.isGreaterThan
 import assertk.assertions.isNotNull
 import assertk.assertions.isTrue
 import mu.KotlinLogging
+import net.postchain.chain0.cm_api.cmGetClusterInfo
 import net.postchain.chain0.common.init.initOperation
 import net.postchain.chain0.common.operations.addNodeToClusterOperation
+import net.postchain.chain0.common.operations.registerNodeWithUnitsOperation
 import net.postchain.chain0.common.operations.updateNodeWithUnitsOperation
+import net.postchain.chain0.common.queries.getBlockchainInfo
 import net.postchain.chain0.common.queries.getBlockchains
 import net.postchain.chain0.common.queries.getContainerData
 import net.postchain.chain0.common.queries.getNodeData
@@ -28,13 +32,20 @@ import net.postchain.chain0.economy_chain.registerProviderAccountOperation
 import net.postchain.chain0.economy_chain.transferToPoolOperation
 import net.postchain.chain0.economy_chain.upgradeContainerOperation
 import net.postchain.chain0.economy_chain_in_directory_chain.initEconomyChainOperation
+import net.postchain.chain0.model.BlockchainState
 import net.postchain.chain0.model.ContainerState
+import net.postchain.chain0.model.ProviderInfo
+import net.postchain.chain0.model.ProviderTier
 import net.postchain.chain0.nm_api.nmGetContainerLimits
+import net.postchain.chain0.proposal_blockchain.BlockchainAction
+import net.postchain.chain0.proposal_blockchain.proposeBlockchainActionOperation
+import net.postchain.chain0.proposal_provider.proposeProvidersOperation
 import net.postchain.client.core.PostchainClient
 import net.postchain.common.BlockchainRid
 import net.postchain.common.hexStringToByteArray
 import net.postchain.common.toHex
 import net.postchain.crypto.KeyPair
+import net.postchain.d1.rell.anchoring_chain_common.getLastAnchoredBlock
 import net.postchain.dapp.PostchainContainer
 import net.postchain.dapp.postTransactionUntilConfirmed
 import net.postchain.gtv.GtvEncoder
@@ -71,15 +82,16 @@ class Directory1EconomyChainMixIT {
     companion object : ManagedModeBase() {
 
         private const val EC_NAME = "economy_chain"
-        private const val APP_CLUSTER = "appCluster"
+        private const val APP_CLUSTER1 = "appCluster1"
+        private const val APP_CLUSTER2 = "appCluster2"
         private const val CONTAINER_UNITS = 2L
         private const val CLUSTER_CLASS = ""
         private const val DURATION_WEEKS = 1L
         private const val EXTRA_STORAGE_GIB = 0L
 
-        private val node1Logger = KotlinLogging.logger("TC_Node1Logger")
-        private val node2Logger = KotlinLogging.logger("TC_Node2Logger")
-        private val node3Logger = KotlinLogging.logger("TC_Node3Logger")
+        private val node1Logger = KotlinLogging.logger("EC_Node1Logger")
+        private val node2Logger = KotlinLogging.logger("EC_Node2Logger")
+        private val node3Logger = KotlinLogging.logger("EC_Node3Logger")
 
         private val node1KeyPair = KeyPair.of(
                 "03ECD350EEBC617CBBFBEF0A1B7AE553A748021FD65C7C50C5ABB4CA16D4EA5B05",
@@ -89,6 +101,9 @@ class Directory1EconomyChainMixIT {
         lateinit var userClient: PostchainClient
         lateinit var userAuthenticator: FTAuthenticator
         lateinit var containerName: String
+        lateinit var dappBrid: BlockchainRid
+        lateinit var CAC1: BlockchainRid
+        lateinit var CAC2: BlockchainRid
 
         init {
             chain0Config = this::class.java.getResource("/directory1deployment/mainnet.xml")!!.readText()
@@ -137,19 +152,34 @@ class Directory1EconomyChainMixIT {
             assertThat(getNodeData(node1.nodeKeyPair.pubKey).active).isTrue()
         }
         assertAnchoringChainProperties()
+
+        // Adding provider2 as system
+        testLogger.info("Adding system provider provider2 its node")
+        val newProviders = listOf(
+                ProviderInfo(node2.provider.pubKey.wData, "provider2", "http://provider2.com")
+        )
+        node1.client(chain0Brid, listOf(node1.provider, node2.provider)).transactionBuilder().addNop()
+                .proposeProvidersOperation(node1.providerPubkey, newProviders, ProviderTier.NODE_PROVIDER, system = true, active = true, description = "")
+                .registerNodeWithUnitsOperation(node2.providerPubkey, node2.pubkey.data, node2.nodeHost, node2.nodePort.toLong(), node2.nodeApiPath(), listOf(systemCluster), 2)
+                .postTransactionUntilConfirmed("System provider2 registered, node2 added to the system cluster")
     }
 
     @Test
     @Order(2)
-    fun `Add new cluster`() {
-        testLogger.info("Adding cluster")
-        with(node1.c0) {
-            transactionBuilder()
-                    .createClusterOperation(node1.providerPubkey, APP_CLUSTER, "SYSTEM_P", listOf(node1.providerPubkey))
-                    .updateNodeWithUnitsOperation(node1.providerPubkey, node1.pubkey.data, null, null, null, 3)
-                    .addNodeToClusterOperation(node1.providerPubkey, node1.pubkey.data, APP_CLUSTER)
-                    .postTransactionUntilConfirmed("$APP_CLUSTER cluster created")
-        }
+    fun `Add clusters`() {
+        testLogger.info("Adding clusters")
+        node1.client(chain0Brid, listOf(node1.provider, node2.provider)).transactionBuilder().addNop()
+                // APP_CLUSTER1 / node1
+                .createClusterOperation(node1.providerPubkey, APP_CLUSTER1, "SYSTEM_P", listOf(node1.providerPubkey))
+                .updateNodeWithUnitsOperation(node1.providerPubkey, node1.pubkey.data, null, null, null, 3)
+                .addNodeToClusterOperation(node1.providerPubkey, node1.pubkey.data, APP_CLUSTER1)
+                // APP_CLUSTER2 / node2
+                .createClusterOperation(node2.providerPubkey, APP_CLUSTER2, "SYSTEM_P", listOf(node2.providerPubkey))
+                .updateNodeWithUnitsOperation(node2.providerPubkey, node2.pubkey.data, null, null, null, 3)
+                .addNodeToClusterOperation(node2.providerPubkey, node2.pubkey.data, APP_CLUSTER2)
+                .postTransactionUntilConfirmed("$APP_CLUSTER1, $APP_CLUSTER2 clusters created")
+        CAC1 = BlockchainRid(node1.c0.cmGetClusterInfo(APP_CLUSTER1).anchoringChain)
+        CAC2 = BlockchainRid(node1.c0.cmGetClusterInfo(APP_CLUSTER2).anchoringChain)
     }
 
     @Test
@@ -229,7 +259,8 @@ class Directory1EconomyChainMixIT {
         userAuthenticator.verifyOperationAuthFlags("create_container")
         val tcRid = userClient.transactionBuilder()
                 .also { userAuthenticator.ftAuth(it) }
-                .createContainerOperation(node1.provider.pubKey.data, CONTAINER_UNITS, CLUSTER_CLASS, DURATION_WEEKS, EXTRA_STORAGE_GIB, APP_CLUSTER, true)
+                .createContainerOperation(
+                        node1.provider.pubKey.data, CONTAINER_UNITS, CLUSTER_CLASS, DURATION_WEEKS, EXTRA_STORAGE_GIB, APP_CLUSTER1, true)
                 .sign(ecUserAccountSigMaker)
                 .postTransactionUntilConfirmed("Create Container")
                 .txRid
@@ -243,7 +274,7 @@ class Directory1EconomyChainMixIT {
         val leaseDataList = userClient.getLeasesByAccount(userAuthenticator.accountId)
         assertThat(leaseDataList.size).isEqualTo(1)
         val leaseData = leaseDataList[0]
-        assertThat(leaseData.clusterName).isEqualTo(APP_CLUSTER)
+        assertThat(leaseData.clusterName).isEqualTo(APP_CLUSTER1)
         assertThat(leaseData.containerUnits).isEqualTo(CONTAINER_UNITS)
         assertThat(leaseData.extraStorageGib).isEqualTo(EXTRA_STORAGE_GIB)
         assertThat(leaseData.expired).isFalse()
@@ -252,7 +283,7 @@ class Directory1EconomyChainMixIT {
         containerName = leaseData.containerName
         val containerData = node1.c0.getContainerData(leaseData.containerName)
         assertThat(containerData).isNotNull()
-        assertThat(containerData.cluster).isEqualTo(APP_CLUSTER)
+        assertThat(containerData.cluster).isEqualTo(APP_CLUSTER1)
         assertThat(containerData.proposedByPubkey).isEqualTo(node1KeyPair.pubKey.wData)
         assertThat(containerData.state).isEqualTo(ContainerState.RUNNING)
 
@@ -262,12 +293,39 @@ class Directory1EconomyChainMixIT {
 
     @Test
     @Order(7)
+    fun `Deploy dapp`() {
+        testLogger.info("Deploying dapp to c1")
+        deployDapp("test_dapp", containerName, assertSigners = arrayOf(node1))
+        dappBrid = dapps["test_dapp"]!!
+
+        // Making sure about 5 blocks of dapp are built
+        val dappClient = node1.client(dappBrid)
+        val height = dappClient.currentBlockHeight()
+        awaitUntilAsserted {
+            assertThat(dappClient.currentBlockHeight()).isGreaterThan(height + 4)
+        }
+    }
+
+    @Test
+    @Order(8)
     fun `Upgrade container`() {
         testLogger.info("Upgrade container")
+
+        // Container upgrade will cause container migration (all blockchains will be moved to a new container),
+        // so we need to pause all blockchains.
+        node1.c0.transactionBuilder().addNop()
+                .proposeBlockchainActionOperation(node1.providerPubkey, dappBrid, BlockchainAction.pause, "")
+                .postTransactionUntilConfirmed("test_dapp paused")
+        // Verify blockchain is PAUSED and all blocks are anchored
+        verifyBlockchainState(node1, dappBrid, BlockchainState.PAUSED)
+        val lastHeightBeforeMoving = node1.client(dappBrid).currentBlockHeight()
+
+        // Upgrading the container in a way that all blockchains to be moved to APP_CLUSTER_2
         userAuthenticator.verifyOperationAuthFlags("upgrade_container")
         val tcRid = userClient.transactionBuilder()
                 .also { userAuthenticator.ftAuth(it) }
-                .upgradeContainerOperation(containerName, CONTAINER_UNITS + 1, CLUSTER_CLASS, EXTRA_STORAGE_GIB, APP_CLUSTER)
+                .upgradeContainerOperation(
+                        containerName, CONTAINER_UNITS + 1, CLUSTER_CLASS, EXTRA_STORAGE_GIB, APP_CLUSTER2)
                 .sign(ecUserAccountSigMaker)
                 .postTransactionUntilConfirmed("Upgrade Container")
                 .txRid
@@ -281,10 +339,46 @@ class Directory1EconomyChainMixIT {
         val leaseDataList = userClient.getLeasesByAccount(userAuthenticator.accountId)
         assertThat(leaseDataList.size).isEqualTo(1)
         val leaseData = leaseDataList[0]
-        assertThat(leaseData.clusterName).isEqualTo(APP_CLUSTER)
+        assertThat(leaseData.clusterName).isEqualTo(APP_CLUSTER2)
         assertThat(leaseData.containerUnits).isEqualTo(CONTAINER_UNITS + 1)
 
+        val newContainerName = containerName + "_new" // TODO: Improve new name
+        assertThat(leaseData.containerName).isEqualTo(newContainerName)
+        val containerData = node1.c0.getContainerData(leaseData.containerName)
+        assertThat(containerData).isNotNull()
+        assertThat(containerData.cluster).isEqualTo(APP_CLUSTER2)
+        assertThat(containerData.proposedByPubkey).isEqualTo(node1KeyPair.pubKey.wData)
+        assertThat(containerData.state).isEqualTo(ContainerState.RUNNING)
+
         val containerLimits = node1.c0.nmGetContainerLimits(leaseData.containerName)
-        assertThat(containerLimits["container_units"]).isEqualTo(CONTAINER_UNITS + 1)
+        assertThat(containerLimits["container_units"]).isEqualTo(1)
+
+        // Resuming blockchain
+        node1.c0.transactionBuilder().addNop()
+                .proposeBlockchainActionOperation(node1.providerPubkey, dappBrid, BlockchainAction.resume, "")
+                .postTransactionUntilConfirmed("test_dapp resumed")
+
+        // Verify blockchain is RUNNING and all blocks are anchored
+        verifyBlockchainState(node1, dappBrid, BlockchainState.RUNNING)
+        val bcInfo = node2.c0.getBlockchainInfo(dappBrid.data)
+        assertThat(bcInfo?.cluster).isEqualTo(APP_CLUSTER2)
+        assertThat(bcInfo?.container).isEqualTo(newContainerName)
+
+        // Asserting that all blocks (some of them) are re-anchored on appCluster2's CAC chain
+        assertBlockReanchored(dappBrid, node1, CAC1, node2, CAC2, 0)
+        assertBlockReanchored(dappBrid, node1, CAC1, node2, CAC2)
+
+        // Making sure a few new blocks of dapp are built
+        val dappClient = node2.client(dappBrid)
+        val height = dappClient.currentBlockHeight()
+        awaitUntilAsserted {
+            assertThat(dappClient.currentBlockHeight()).isGreaterThan(height + 2)
+        }
+
+        // Asserting that new blocks are anchored on s2SAC chain
+        awaitUntilAsserted {
+            val lastAnchoredHeight2 = node2.client(CAC2).getLastAnchoredBlock(dappBrid)!!.blockHeight
+            assertThat(lastAnchoredHeight2).isGreaterThan(lastHeightBeforeMoving)
+        }
     }
 }
