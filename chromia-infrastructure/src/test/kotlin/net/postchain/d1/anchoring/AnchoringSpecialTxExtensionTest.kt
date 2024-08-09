502b25066d439acdef8af265dc545bec31f3ad07
Eugene Tykulov <eugene.tykulov@chromaway.com>
2024-08-09 15:54:42 +0300

    Add `read two ranges from pipe1 and part of the first range from pipe2 until size limit is reached()` test

--- chromia-infrastructure/src/test/kotlin/net/postchain/d1/anchoring/AnchoringSpecialTxExtensionTest.kt
+++ chromia-infrastructure/src/test/kotlin/net/postchain/d1/anchoring/AnchoringSpecialTxExtensionTest.kt
@@ -28,11 +28,11 @@ class AnchoringSpecialTxExtensionTest {
     private lateinit var module0: GTXModule
 
     // chain1
-    private val blockchainRid1 = BlockchainRid.buildRepeat(1)
+    private val brid1 = BlockchainRid.buildRepeat(1)
     private lateinit var pipe1: AnchoringPipe
 
     // chain2
-    private val blockchainRid2 = BlockchainRid.buildRepeat(2)
+    private val brid2 = BlockchainRid.buildRepeat(2)
     private lateinit var pipe2: AnchoringPipe
 
     private var mightHaveNewPackets = true
@@ -41,17 +41,19 @@ class AnchoringSpecialTxExtensionTest {
     fun beforeEach() {
 
         pipe1 = mock {
-            on { blockchainRid } doReturn blockchainRid1
+            on { chainID } doReturn 1
+            on { blockchainRid } doReturn brid1
             on { mightHaveNewPackets() } doReturn mightHaveNewPackets
         }
 
         pipe2 = mock {
-            on { blockchainRid } doReturn blockchainRid2
+            on { chainID } doReturn 2
+            on { blockchainRid } doReturn brid2
             on { mightHaveNewPackets() } doReturn mightHaveNewPackets
         }
 
         val anchoringReceiver: AnchoringReceiver = mock {
-            on { getRelevantPipes() } doReturn listOf(pipe1, pipe1)
+            on { getRelevantPipes() } doReturn listOf(pipe1, pipe2)
         }
 
         anchoringReceiverFactory = mock {
@@ -67,31 +69,31 @@ class AnchoringSpecialTxExtensionTest {
         sut.createReceiver(blockchainRid0)
 
         module0 = mock {
-            on { query(any(), eq("get_last_anchored_block"), eq(gtv("blockchain_rid" to gtv(blockchainRid0)))) } doReturn gtv(
-                    "block_rid" to gtv(blockchainRid1),
+            on { query(any(), eq("get_last_anchored_block"), eq(gtv("blockchain_rid" to gtv(brid1)))) } doReturn gtv(
+                    "block_rid" to gtv(brid1),
                     "block_height" to gtv(0)
             )
-            on { query(any(), eq("get_last_anchored_block"), eq(gtv("blockchain_rid" to gtv(blockchainRid1)))) } doReturn gtv(
-                    "block_rid" to gtv(blockchainRid2),
+            on { query(any(), eq("get_last_anchored_block"), eq(gtv("blockchain_rid" to gtv(brid2)))) } doReturn gtv(
+                    "block_rid" to gtv(brid2),
                     "block_height" to gtv(0)
             )
         }
         sut.init(module0, 0L, blockchainRid0, mock())
     }
 
-    private fun generatePackets(blocks: IntRange) = blocks.map {
+    private fun generatePackets(blocks: IntRange, brid: BlockchainRid) = blocks.map {
         AnchoringPacket(
                 it.toLong(),
                 GtvEncoder.encodeGtv(gtv("blockRid - %010d".format(it))),
-                GtvEncoder.encodeGtv(gtv("header - %010d".format(it))),
+                GtvEncoder.encodeGtv(gtv("header - $brid - %010d".format(it))),
                 byteArrayOf())
     }
 
     @Test
     fun `read a few packets from the first range from pipe1 until size limit is reached`() {
         // 40 packets are available
-        val range0 = generatePackets(0 until 20)
-        val range1 = generatePackets(20 until 40)
+        val range0 = generatePackets(0 until 20, brid1)
+        val range1 = generatePackets(20 until 40, brid1)
         whenever(pipe1.fetchNextRange(any(), any())).doReturn(range0, range1)
 
         // read 3 out of 40 packets
@@ -108,12 +110,12 @@ class AnchoringSpecialTxExtensionTest {
 
     @Test
     fun `read entire first range and part of second range from pipe1 until size limit is reached`() {
-        // 50 packets are available
-        val range0 = generatePackets(0 until 20)
-        val range1 = generatePackets(20 until 40)
+        // 40 packets are available
+        val range0 = generatePackets(0 until 20, brid1)
+        val range1 = generatePackets(20 until 40, brid1)
         whenever(pipe1.fetchNextRange(any(), any())).doReturn(range0, range1)
 
-        // read 23 (MAX_PACKETS_PER_REQUEST + 3) out of 50 packets
+        // read 23 (MAX_PACKETS_PER_REQUEST + 3) out of 40 packets
         val sizeOf23 = (range0 + range1).take(23).sumOf { sut.buildOpData(it).second }
         sut.maxTxSize = sizeOf23.toLong() + TX_SIZE_MARGIN
 
@@ -123,5 +125,30 @@ class AnchoringSpecialTxExtensionTest {
         assertThat(ops.size).isEqualTo(23)
         verify(pipe1, times(2)).fetchNextRange(any(), any())
         verify(pipe2, never()).fetchNextRange(any(), any())
+    }
+
+    @Test
+    fun `read two ranges from pipe1 and part of the first range from pipe2 until size limit is reached`() {
+        // chain1 / 40 packets are available
+        val range10 = generatePackets(0 until 20, brid1)
+        val range11 = generatePackets(20 until 40, brid1)
+        whenever(pipe1.fetchNextRange(any(), any())).doReturn(range10, range11, emptyList())
+
+        // chain2 / 40 packets are available
+        val range20 = generatePackets(0 until 20, brid2)
+        val range21 = generatePackets(20 until 40, brid2)
+        whenever(pipe2.fetchNextRange(any(), any())).doReturn(range20, range21, emptyList())
+
+        // read 45 packets in total
+        val sizeOf45 = (range10 + range11 + range20 + range21).take(45).sumOf { sut.buildOpData(it).second }
+        sut.maxTxSize = sizeOf45.toLong() + TX_SIZE_MARGIN
+
+        val ops = sut.createSpecialOperations(SpecialTransactionPosition.Begin, mock())
+
+        // 23 packages will fit in total size
+        assertThat(ops.size).isEqualTo(45)
+        // 3 times b/c mightHaveNewPackets = true
+        verify(pipe1, times(3)).fetchNextRange(any(), any())
+        verify(pipe2, times(1)).fetchNextRange(any(), any())
     }
 }
