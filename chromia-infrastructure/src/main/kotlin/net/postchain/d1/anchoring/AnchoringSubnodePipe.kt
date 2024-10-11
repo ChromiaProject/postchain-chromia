c9aa9d16fc69c057df4202f2de7f40683bf20ad9
Mikael Ståldal <mikael.staldal@chromaway.com>
2024-10-11 14:50:43 +0000

    POS-1539: Anchoring: fetch packets from subnodes asynchronously

--- chromia-infrastructure/src/main/kotlin/net/postchain/d1/anchoring/AnchoringSubnodePipe.kt
+++ chromia-infrastructure/src/main/kotlin/net/postchain/d1/anchoring/AnchoringSubnodePipe.kt
@@ -2,49 +2,134 @@
 
 package net.postchain.d1.anchoring
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder
 import mu.KLogging
+import mu.withLoggingContext
 import net.postchain.common.BlockchainRid
+import net.postchain.common.exception.UserMistake
 import net.postchain.core.BlockEContext
+import net.postchain.core.block.BlockQueries
+import net.postchain.d1.anchoring.AnchoringPipe.Companion.MAX_PACKETS_PER_REQUEST
 import net.postchain.d1.query.MasterClient
+import net.postchain.gtv.GtvDictionary
+import net.postchain.gtv.GtvFactory.gtv
+import net.postchain.gtv.GtvInteger
+import net.postchain.logging.BLOCKCHAIN_RID_TAG
+import net.postchain.logging.CHAIN_IID_TAG
 import net.postchain.network.mastersub.master.MasterConnectionManager
 import java.lang.Long.max
+import java.util.concurrent.ConcurrentSkipListMap
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledFuture
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicLong
+import kotlin.time.Duration.Companion.seconds
 
 class AnchoringSubnodePipe(
         override val chainID: Long,
         override val blockchainRid: BlockchainRid,
-        connectionManager: MasterConnectionManager
+        connectionManager: MasterConnectionManager,
+        private val anchorBlockQueriesProvider: () -> BlockQueries
 ) : AnchoringPipe {
-    private val highestSeen = AtomicLong(-1L)
-    private val lastCommitted = AtomicLong(-1L)
+    private val highestFetched = AtomicLong(-1L)
+    private val packets = ConcurrentSkipListMap<Long, AnchoringPacket>() // block height to packet
 
-    companion object : KLogging()
+    companion object : KLogging() {
+        val pollInterval = 60.seconds
+    }
 
     private val client: MasterClient = MasterClient(connectionManager.masterSubQueryManager, blockchainRid)
 
-    override fun setHighestSeenHeight(height: Long) = highestSeen.set(height)
+    private val executor = Executors.newSingleThreadScheduledExecutor(
+            ThreadFactoryBuilder().setNameFormat(
+                    "subnode-anchoring-pipe-worker-chainID-$chainID-blockchainRid-${blockchainRid.toHex()}"
+            ).build()
+    )
 
-    override fun mightHaveNewPackets() = highestSeen.get() > lastCommitted.get()
+    @Volatile
+    private var currentTask: ScheduledFuture<*> =
+            executor.schedule(::tryFetchBlocksAndReschedule, pollInterval.inWholeMilliseconds, TimeUnit.MILLISECONDS)
 
-    override fun numberOfNewPackets() = highestSeen.get() - lastCommitted.get()
-
-    override fun fetchNextRange(fromHeight: Long, limit: Long): List<AnchoringPacket> = try {
-        client.blocksFromHeight(fromHeight, limit).map {
-            AnchoringPacket(
-                    height = it.blockHeight,
-                    blockRid = it.blockRid,
-                    rawHeader = it.blockHeader,
-                    rawWitness = it.witness,
-            )
+    override fun newBlockAvailable(height: Long) {
+        if (height > highestFetched.get()) { // Trigger a fetch since we don't already have this block
+            currentTask.cancel(false)
+            currentTask = executor.schedule(::tryFetchBlocksAndReschedule, 0L, TimeUnit.MILLISECONDS)
         }
-    } catch (e: Exception) {
-        logger.warn(e) { "Fetching the block range starting from height $fromHeight for $blockchainRid from the subnode failed: $e" }
-        listOf()
     }
 
-    override fun markTaken(currentPointer: Long, bctx: BlockEContext) {
-        bctx.addAfterCommitHook {
-            lastCommitted.getAndUpdate { max(it, currentPointer) }
+    override fun mightHaveNewPackets() = packets.isNotEmpty()
+
+    override fun numberOfNewPackets() = packets.size.toLong()
+
+    private fun tryFetchBlocksAndReschedule() {
+        withLoggingContext(mapOf(
+                CHAIN_IID_TAG to chainID.toString(),
+                BLOCKCHAIN_RID_TAG to blockchainRid.toHex()
+        )) {
+            try {
+                logger.debug { "Fetching blocks" }
+                fetchBlocks()
+                logger.debug { "Fetched blocks" }
+            } catch (e: UserMistake) {
+                logger.warn(e.message)
+            } catch (e: Exception) {
+                logger.error("Block fetch failed: ${e.message}", e)
+            }
         }
+        currentTask.cancel(false)
+        currentTask = executor.schedule(::tryFetchBlocksAndReschedule, pollInterval.inWholeMilliseconds, TimeUnit.MILLISECONDS)
+    }
+
+    private fun fetchBlocks() {
+        val highestFetchHeight = highestFetched.get()
+        val lastHeight = if (highestFetchHeight < 0) {
+            fetchLastAnchoredHeight()
+        } else {
+            highestFetchHeight
+        }
+        fetchBlocksFromHeight(lastHeight + 1)
+    }
+
+    private fun fetchLastAnchoredHeight(): Long =
+            anchorBlockQueriesProvider().query("get_last_anchored_block", gtv(mapOf("blockchain_rid" to gtv(blockchainRid))))
+                    .toCompletableFuture().get()
+                    .let { v0 -> ((v0 as? GtvDictionary)?.dict?.get("block_height") as? GtvInteger)?.integer ?: -1 }
+
+    private fun fetchBlocksFromHeight(fromHeight: Long) {
+        val fetchedPackets = try {
+            client.blocksFromHeight(fromHeight, MAX_PACKETS_PER_REQUEST).map {
+                AnchoringPacket(
+                        height = it.blockHeight,
+                        blockRid = it.blockRid,
+                        rawHeader = it.blockHeader,
+                        rawWitness = it.witness,
+                )
+            }
+        } catch (e: Exception) {
+            logger.warn(e) { "Fetching the block range starting from height $fromHeight for $blockchainRid from the subnode failed: $e" }
+            listOf()
+        }
+
+        var maxHeight = -1L
+        for (packet in fetchedPackets) {
+            maxHeight = max(maxHeight, packet.height)
+            packets[packet.height] = packet
+        }
+        highestFetched.getAndUpdate { max(it, maxHeight) }
+    }
+
+    override fun fetchNextRange(fromHeight: Long): List<AnchoringPacket> =
+            packets.tailMap(fromHeight, true).values.toList()
+
+    override fun markTaken(lastCommittedHeight: Long, bctx: BlockEContext) {
+        bctx.addAfterCommitHook {
+            for (height in packets.headMap(lastCommittedHeight, true).keys) {
+                packets.remove(height)
+            }
+        }
+    }
+
+    override fun shutdown() {
+        executor.shutdownNow()
     }
 }
--- chromia-infrastructure/src/test/kotlin/net/postchain/d1/anchoring/AnchoringSpecialTxExtensionTest.kt
+++ chromia-infrastructure/src/test/kotlin/net/postchain/d1/anchoring/AnchoringSpecialTxExtensionTest.kt
@@ -94,7 +94,7 @@ class AnchoringSpecialTxExtensionTest {
         // 40 packets are available
         val range0 = generatePackets(0 until 20, brid1)
         val range1 = generatePackets(20 until 40, brid1)
-        whenever(pipe1.fetchNextRange(any(), any())).doReturn(range0, range1)
+        whenever(pipe1.fetchNextRange(any())).doReturn(range0, range1)
 
         // read 3 out of 40 packets
         val multiOpAnchoringSpecialTxBuilder = MultiOpAnchoringSpecialTxBuilder()
@@ -105,8 +105,8 @@ class AnchoringSpecialTxExtensionTest {
 
         // 3 packages will fit in total size
         assertThat(ops.size).isEqualTo(3)
-        verify(pipe1, times(1)).fetchNextRange(any(), any())
-        verify(pipe2, never()).fetchNextRange(any(), any())
+        verify(pipe1, times(1)).fetchNextRange(any())
+        verify(pipe2, never()).fetchNextRange(any())
     }
 
     @Test
@@ -114,7 +114,7 @@ class AnchoringSpecialTxExtensionTest {
         // 40 packets are available
         val range0 = generatePackets(0 until 20, brid1)
         val range1 = generatePackets(20 until 40, brid1)
-        whenever(pipe1.fetchNextRange(any(), any())).doReturn(range0, range1)
+        whenever(pipe1.fetchNextRange(any())).doReturn(range0, range1)
 
         // read 23 (MAX_PACKETS_PER_REQUEST + 3) out of 40 packets
         val multiOpAnchoringSpecialTxBuilder = MultiOpAnchoringSpecialTxBuilder()
@@ -125,8 +125,8 @@ class AnchoringSpecialTxExtensionTest {
 
         // 23 packages will fit in total size
         assertThat(ops.size).isEqualTo(23)
-        verify(pipe1, times(2)).fetchNextRange(any(), any())
-        verify(pipe2, never()).fetchNextRange(any(), any())
+        verify(pipe1, times(2)).fetchNextRange(any())
+        verify(pipe2, never()).fetchNextRange(any())
     }
 
     @Test
@@ -134,12 +134,12 @@ class AnchoringSpecialTxExtensionTest {
         // chain1 / 40 packets are available
         val range10 = generatePackets(0 until 20, brid1)
         val range11 = generatePackets(20 until 40, brid1)
-        whenever(pipe1.fetchNextRange(any(), any())).doReturn(range10, range11, emptyList())
+        whenever(pipe1.fetchNextRange(any())).doReturn(range10, range11, emptyList())
 
         // chain2 / 40 packets are available
         val range20 = generatePackets(0 until 20, brid2)
         val range21 = generatePackets(20 until 40, brid2)
-        whenever(pipe2.fetchNextRange(any(), any())).doReturn(range20, range21, emptyList())
+        whenever(pipe2.fetchNextRange(any())).doReturn(range20, range21, emptyList())
 
         // read 45 packets in total
         val multiOpAnchoringSpecialTxBuilder = MultiOpAnchoringSpecialTxBuilder()
@@ -151,8 +151,8 @@ class AnchoringSpecialTxExtensionTest {
         // 45 packages will fit in total size
         assertThat(ops.size).isEqualTo(45)
         // 3 times b/c mightHaveNewPackets = true
-        verify(pipe1, times(3)).fetchNextRange(any(), any())
-        verify(pipe2, times(1)).fetchNextRange(any(), any())
+        verify(pipe1, times(3)).fetchNextRange(any())
+        verify(pipe2, times(1)).fetchNextRange(any())
     }
 
     @Test
@@ -163,12 +163,12 @@ class AnchoringSpecialTxExtensionTest {
         // chain1 / 40 packets are available
         val range10 = generatePackets(0 until 20, brid1)
         val range11 = generatePackets(20 until 40, brid1)
-        whenever(pipe1.fetchNextRange(any(), any())).doReturn(range10, range11, emptyList())
+        whenever(pipe1.fetchNextRange(any())).doReturn(range10, range11, emptyList())
 
         // chain2 / 30 packets are available
         val range20 = generatePackets(0 until 20, brid2)
         val range21 = generatePackets(20 until 30, brid2)
-        whenever(pipe2.fetchNextRange(any(), any())).doReturn(range20, range21, emptyList())
+        whenever(pipe2.fetchNextRange(any())).doReturn(range20, range21, emptyList())
 
         // we can read all the packages
         val multiOpAnchoringSpecialTxBuilder = MultiOpAnchoringSpecialTxBuilder()
@@ -179,8 +179,8 @@ class AnchoringSpecialTxExtensionTest {
 
         // 63 packages will fit in total size
         assertThat(ops.size).isEqualTo(33 + 30)
-        verify(pipe1, times(2)).fetchNextRange(any(), any())
+        verify(pipe1, times(2)).fetchNextRange(any())
         // 3 times b/c mightHaveNewPackets = true
-        verify(pipe2, times(3)).fetchNextRange(any(), any())
+        verify(pipe2, times(3)).fetchNextRange(any())
     }
 }
