7e9f8c20627b03181c0c11ba267074da86207f6c
Johan Nilsson <johan.nilsson@chromaway.com>
2025-01-28 14:54:21 +0100

    Support subnode PG migration

--- docker-images/chromia-subnode/src/main/jib/opt/chromaway/postchain/entrypoint.sh
+++ docker-images/chromia-subnode/src/main/jib/opt/chromaway/postchain/entrypoint.sh
@@ -6,6 +6,8 @@ set -eu
 # Percentage of total memory to dedicate to java/psql (30% buffer with current setup)
 JAVA_MEMORY_SHARE=35
 export PSQL_MEMORY_SHARE=35
+export ALREADY_INITED_FILE=".db-initialized"
+MIGRATE_POSTGRES=${MIGRATE_POSTGRES:-false}
 
 # Exit container if run as root
 if [ "$(id -u)" = '0' ]; then
@@ -16,6 +18,13 @@ fi
 trap 'echo "Shutting down..." ; kill ${POSTCHAIN_PID} ; pg_ctl stop -m smart' TERM INT
 
 echo "Configuring and starting Postgres"
+if [ "$MIGRATE_POSTGRES" = "true" ] && [ -f "$PGDATA/$ALREADY_INITED_FILE" ]; then
+  bash postgres-migrate.sh
+  if [ -z "$(ls -A $PGDATA)" ]; then
+    echo "No data found in PGDATA - this is normal for a successful migration, exiting container to have it restarted"
+    exit 1
+  fi
+fi
 bash postgres-entrypoint.sh postgres
 
 java -Duser.language=en -Duser.country=US -XX:+UnlockDiagnosticVMOptions -XX:AbortVMOnException=java.lang.OutOfMemoryError -XX:MaxRAMPercentage=$JAVA_MEMORY_SHARE -classpath "$POSTCHAIN_DIR/libs/*:$POSTCHAIN_DIR/classpath/*" net.postchain.server.AppKt run-subnode &
