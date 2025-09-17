27566d51c080421cf608c6ef6d5a770f5391ae1c
Mikael Ståldal <mikael.staldal@chromaway.com>
2025-09-17 17:07:22 +0200

    POS-1926: GTX Crypto ops for Ethereum

---
+++ chromia-infrastructure/src/main/kotlin/net/postchain/crypto/CryptoOpsGTXModule.kt
@@ -0,0 +1,20 @@
+package net.postchain.crypto
+
+import net.postchain.core.EContext
+import net.postchain.gtx.GTXModuleMetadata
+import net.postchain.gtx.MetadataProvider
+import net.postchain.gtx.SimpleGTXModule
+
+class CryptoOpsGTXModule : SimpleGTXModule<Unit>(
+        Unit,
+        mapOf(CheckSigERC191Personal.OP_NAME to ::CheckSigERC191Personal),
+        mapOf()
+), MetadataProvider {
+    override fun initializeDB(ctx: EContext) {}
+
+    override fun getMetadata() = GTXModuleMetadata(
+            operations = mapOf(
+                    CheckSigERC191Personal.OP_NAME to CheckSigERC191Personal.metadata,
+            ),
+            queries = mapOf())
+}
