207a410de82ce6201cde1da51e1f1d1227d444f5
Mikael Ståldal <mikael.staldal@chromaway.com>
2025-09-24 12:01:21 +0200

    Add alg parameter to gtxc.checksig_webauthn_authenticate operation

--- chromia-infrastructure/src/main/kotlin/net/postchain/crypto/webauthn/CheckSigWebAuthnAuthenticate.kt
+++ chromia-infrastructure/src/main/kotlin/net/postchain/crypto/webauthn/CheckSigWebAuthnAuthenticate.kt
@@ -12,21 +12,31 @@ import net.postchain.gtx.OperationMetadata
 import net.postchain.gtx.data.ExtOpData
 import java.security.GeneralSecurityException
 import java.security.KeyFactory
-import java.security.PublicKey
 import java.security.Signature
 import java.security.spec.X509EncodedKeySpec
 
 /**
  * WebAuthn authenticate.
+ * https://developer.mozilla.org/en-US/docs/Web/API/Web_Authentication_API#authenticating_a_user
  */
 class CheckSigWebAuthnAuthenticate(conf: Unit, opData: ExtOpData) : GTXOperation(opData) {
     companion object : KLogging() {
         const val OP_NAME = "gtxc.checksig_webauthn_authenticate"
 
         val metadata = OperationMetadata(args = listOf(
+                // https://developer.mozilla.org/en-US/docs/Web/API/AuthenticatorAssertionResponse/authenticatorData from AuthenticatorAssertionResponse
                 ArgumentMetadata("authenticator_data", setOf(GtvType.BYTEARRAY)),
+
+                // https://developer.mozilla.org/en-US/docs/Web/API/AuthenticatorResponse/clientDataJSON from AuthenticatorAssertionResponse
                 ArgumentMetadata("client_data_json", setOf(GtvType.STRING)),
+
+                // COSE Algorithm Identifier, https://developer.mozilla.org/en-US/docs/Web/API/AuthenticatorAttestationResponse/getPublicKeyAlgorithm
+                ArgumentMetadata("alg", setOf(GtvType.INTEGER)),
+
+                // SubjectPublicKeyInfo, https://developer.mozilla.org/en-US/docs/Web/API/AuthenticatorAttestationResponse/getPublicKey
                 ArgumentMetadata("public_key", setOf(GtvType.BYTEARRAY)),
+
+                // https://developer.mozilla.org/en-US/docs/Web/API/AuthenticatorAssertionResponse/signature from AuthenticatorAssertionResponse
                 ArgumentMetadata("signature", setOf(GtvType.BYTEARRAY)),
         ))
     }
@@ -44,39 +54,46 @@ class CheckSigWebAuthnAuthenticate(conf: Unit, opData: ExtOpData) : GTXOperation
     }
 
     private fun verifySignature(args: Array<out Gtv>) {
-        if (args.size != 4) throw UserMistake("need 4 args, got ${args.size}")
+        if (args.size != 5) throw UserMistake("need 5 args, got ${args.size}")
         val authenticatorData = args[0].asByteArray()
         val clientDataJSON = args[1].asString()
-        val publicKey = args[2].asByteArray()
-        val signature = args[3].asByteArray()
+        val alg = args[2].asInteger()
+        val publicKey = args[3].asByteArray()
+        val signature = args[4].asByteArray()
 
         val signedData = authenticatorData + sha256Digest(clientDataJSON.toByteArray(Charsets.UTF_8))
 
         val pk = try {
             val key = X509EncodedKeySpec(publicKey)
-            val kFact = KeyFactory.getInstance("EC")
+            val kFact = KeyFactory.getInstance(getKeyAlgorithm(alg))
             kFact.generatePublic(key)
-        } catch (_: GeneralSecurityException) {
-            throw UserMistake("invalid public key")
+        } catch (e: GeneralSecurityException) {
+            throw UserMistake("invalid public key: ${e.message}")
         }
 
-        if (!verify(signedData, pk, signature)) {
-            throw UserMistake("signature verification failed")
+        try {
+            val signatureChecker = Signature.getInstance(getSignatureAlgorithm(alg))
+            signatureChecker.initVerify(pk)
+            signatureChecker.update(signedData)
+            if (!signatureChecker.verify(signature)) {
+                throw UserMistake("signature verification failed")
+            }
+        } catch (e: GeneralSecurityException) {
+            throw UserMistake("unable to verify signature: ${e.message}")
+        } catch (e: IllegalArgumentException) {
+            throw UserMistake("unable to verify signature: ${e.message}")
         }
     }
 
-    private fun verify(signedData: ByteArray, publicKey: PublicKey, signature: ByteArray): Boolean {
-        return try {
-            val signatureChecker = Signature.getInstance("SHA256withECDSA")
-            signatureChecker.initVerify(publicKey)
-            signatureChecker.update(signedData)
-            signatureChecker.verify(signature)
-        } catch (e: GeneralSecurityException) {
-            logger.warn(e) { "Unable to verify WebAuthn signature: ${e.message}" }
-            false
-        } catch (e: IllegalArgumentException) {
-            logger.warn(e) { "Unable to verify WebAuthn signature: ${e.message}" }
-            false
-        }
+    private fun getKeyAlgorithm(coseAlgorithm: Long): String = when (coseAlgorithm) {
+        -7L -> "EC"
+        -8L -> "EdDSA"
+        else -> throw UserMistake("unsupported algorithm: $coseAlgorithm")
+    }
+
+    private fun getSignatureAlgorithm(coseAlgorithm: Long): String = when (coseAlgorithm) {
+        -7L -> "SHA256withECDSA"
+        -8L -> "ed25519"
+        else -> throw UserMistake("unsupported algorithm: $coseAlgorithm")
     }
 }
